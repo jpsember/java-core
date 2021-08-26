8f241d4df3a7fad07b96ab99b930a65804fc4b5a
Jeff Sember <jpsember@gmail.com>
2021-08-26 15:10:54 -0700

    Issue #1

--- src/main/java/js/file/Files.java
+++ src/main/java/js/file/Files.java
@@ -864,6 +864,61 @@ public final class Files extends BaseObject {
     //    return BackupManager.get().rebuild(directory, preserveRelativeFiles);
   }
 
+  /**
+   * Find a file within a project directory structure, by ascending to a parent
+   * directory until found (or we run out of parents)
+   * 
+   * @param startParentDirectoryOrNull
+   *          directory to start search within, or null for current directory
+   * @param filename
+   *          name of file (or directory) to look for
+   * @param errorContext
+   *          if nonempty, and file isn't found, throws exception, displaying a
+   *          message with these arguments
+   * @return found file, or Files.DEFAULT
+   */
+  public static File getFileWithinParents(File startParentDirectoryOrNull, String filename,
+      Object... errorContext) {
+    File startDir;
+    if (startParentDirectoryOrNull == null)
+      startDir = Files.currentDirectory();
+    else
+      startDir = Files.absolute(startParentDirectoryOrNull);
+    File result = Files.DEFAULT;
+    File dir = startDir;
+    while (true) {
+      File candidate = new File(dir, filename);
+      if (candidate.exists()) {
+        result = candidate;
+        break;
+      }
+      dir = dir.getParentFile();
+      if (dir == null)
+        break;
+    }
+    if (Files.empty(result) && errorContext.length > 0)
+      throw badArg("Cannot find file", filename, "within", startDir, "; context:",
+          BasePrinter.toString(errorContext));
+    return result;
+  }
+
+  public File projectConfigDirectory() {
+    if (mProjectConfigDirectory == null) {
+      mProjectConfigDirectory = getFileWithinParents(null, "project_config", "project_config directory");
+    }
+    return mProjectConfigDirectory;
+  }
+
+  public File projectSecretsDirectory() {
+    if (mProjectSecretsDirectory == null) {
+      mProjectSecretsDirectory = getFileWithinParents(null, "secrets", "project_config directory");
+    }
+    return mProjectSecretsDirectory;
+  }
+
+  private File mProjectSecretsDirectory;
+  private File mProjectConfigDirectory;
+
   // ------------------------------------------------------------------
   // Streams
   // ------------------------------------------------------------------
