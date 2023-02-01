9d5466f611470259e69363477e9cc82f70d9a7e6
Jeff Sember <jpsember@gmail.com>
2023-01-31 18:46:13 -0800

    Issue #18

--- src/main/java/js/app/ScreenCaptureOper.java
+++ src/main/java/js/app/ScreenCaptureOper.java
@@ -3,28 +3,34 @@ package js.app;
 import static js.base.Tools.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+import js.base.BaseObject;
 import js.base.DateTimeTools;
 import js.base.SystemCall;
 import js.file.DirWalk;
 import js.file.Files;
 
-public class ScreenCaptureOper {
+public class ScreenCaptureOper extends BaseObject {
 
   public static void main(String[] args) {
     new ScreenCaptureOper().perform(args);
   }
 
   private void perform(String[] args) {
+    alertVerbose();
 
     File errFile = null;
     if (mSimulateError) {
       errFile = Files.getDesktopFile("_err_sentinel.txt_");
     }
 
-    if (alert("short interval"))
-      mSecondsBetweenShots = 5;
+    buildImageFiles();
+    buildZipFiles();
 
     // See https://markholloway.com/2018/11/14/macos-screencapture-terminal/
 
@@ -63,7 +69,6 @@ public class ScreenCaptureOper {
 
           File output = getNextOutputFile(timestamp, devNum);
           s.arg(output);
-          s.setVerbose();
           s.call();
 
           // If error output is 'no such device', ignore
@@ -83,35 +88,102 @@ public class ScreenCaptureOper {
   }
 
   private void cullShots() {
-    while (imageFiles().size() > mMaxScreenshots) {
-      File x = imageFiles().get(0);
-      Files.S.deletePeacefully(x);
-      imageFiles().remove(0);
+    // If there are enough image files for a zip file, create one
+    if (imageFiles().size() < mMaxImagesPerZip)
+      return;
+
+    File tmpFile = createZipFile(imageFiles());
+    File zipFile = new File(imageDir(), "_collection_" + System.currentTimeMillis() + "." + Files.EXT_ZIP);
+
+    Files.S.moveFile(tmpFile, zipFile);
+    zipFiles().add(zipFile);
+    Files.S.deleteFile(tmpFile);
+
+    for (File imgFile : imageFiles()) 
+      Files.S.deleteFile(imgFile);
+    imageFiles().clear();
+
+    // Delete old image zip files
+    //
+    while (zipFiles().size() > mMaxScreenshotZipFiles) {
+      File x = zipFiles().get(0);
+      Files.S.deleteFile(x);
+      zipFiles().remove(0);
     }
   }
 
-  private List<File> imageFiles() {
-    if (mImageFiles == null) {
-      List<File> lst = arrayList();
-      DirWalk w = new DirWalk(imageDir());
-      w.withRecurse(false);
-      w.withExtensions("jpg");
-      for (File f : w.files()) {
-        String name = f.getName();
-        if (!name.startsWith("_screenshot_"))
-          continue;
-        lst.add(f);
+  private File createZipFile(List<File> fileList) {
+    File tmpFile = new File(imageDir(), "_tmpfile_." + Files.EXT_TMP);
+    Files.S.deletePeacefully(tmpFile);
+
+    try {
+      ZipOutputStream zipStream = new ZipOutputStream(Files.S.outputStream(tmpFile));
+      int index = INIT_INDEX;
+      for (File absFile : fileList) {
+        index++;
+        String name = absFile.getName();
+        Integer oldMapping = uq.put(name, index);
+        ZipEntry zipEntry = new ZipEntry(name);
+        zipStream.putNextEntry(zipEntry);
+        zipStream.write(Files.toByteArray(absFile, "ScreenCaptureOper add to zip"));
+        zipStream.closeEntry();
       }
-      mImageFiles = lst;
+
+      zipStream.close();
+    } catch (IOException e) {
+      throw Files.asFileException(e);
     }
+
+    return tmpFile;
+  }
+
+  private void buildImageFiles() {
+    List<File> lst = arrayList();
+    DirWalk w = new DirWalk(imageDir());
+    w.withRecurse(false);
+    w.withExtensions("jpg");
+    for (File f : w.files()) {
+      String name = f.getName();
+      if (!name.startsWith("_screenshot_"))
+        continue;
+      lst.add(f);
+    }
+    mImageFiles = lst;
+  }
+
+  private List<File> imageFiles() {
     return mImageFiles;
+  }
+
+  private void buildZipFiles() {
+    List<File> lst = arrayList();
+    DirWalk w = new DirWalk(imageDir());
+    w.withRecurse(false);
+    w.withExtensions("zip");
+    for (File f : w.files()) {
+      String name = f.getName();
+      if (!name.startsWith("_collection_"))
+        continue;
+      lst.add(f);
+    }
+    mZipFiles = lst;
+  }
+
+  private List<File> zipFiles() {
+    return mZipFiles;
   }
 
   private File imageDir() {
     if (mImageDir == null) {
-      File c = Files.S.mkdirs(new File(Files.homeDirectory(), "Downloads/screenshots"));
+      // I used to store things in the Downloads directory, but the files don't show up
+      // when run as a daemon; see https://developer.apple.com/forums/thread/118508
+      //
+      // Using our own subdirectory in the home directory seems to work.
+      //
+      File c = Files.S.mkdirs(new File(Files.homeDirectory(), "_screenshots_"));
       checkState(c.isDirectory(), "can't find directory:", c);
       mImageDir = c;
+      log("set image dir:", mImageDir);
     }
     return mImageDir;
   }
@@ -123,10 +195,12 @@ public class ScreenCaptureOper {
     return f;
   }
 
-  private boolean mSimulateError = true;
+  private boolean mSimulateError = false; //true;
   private List<File> mImageFiles;
-  private int mSecondsBetweenShots = 60;
-  private int mMaxScreenshots = 1500;
+  private List<File> mZipFiles;
+  private int mSecondsBetweenShots = 10; // 60;
+  private int mMaxImagesPerZip = 10; // 100
+  private int mMaxScreenshotZipFiles = 3; // 100
   private long mStartTime;
   private final boolean mDisableActualScreenshots = false && alert("skipping shots");
 
