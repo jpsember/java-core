36acababea05c2856a99b2a9850ad81bfc0664ee
Jeff Sember <jpsember@gmail.com>
2023-11-16 11:36:45 -0800

    Issue #23

--- src/main/java/js/leetcode/P22GenerateParenthesis.java
+++ src/main/java/js/leetcode/P22GenerateParenthesis.java
@@ -1,14 +1,27 @@
-// 22. Generate Parenthesis
+package js.leetcode;
+
+//22. Generate Parenthesis
 
 // Insight: what is a grammar for such parenthesis?
-
+// ...that wasn't productive, but worth further research.
 //
+// The approach I figured out was a nested loop, one for each opening char '('.  
+// 
+// set up cursors
+// while true
+//   generate string based on cursor positions
+//   advance the last cursor; if it has reached its limit, move back to the previous cursor
+//    and repeat.  If we run out of cursors, stop.
+//   initialize next cursors (if we moved back in the above step) to be the previous cursor + 1
+// 
 
-package js.leetcode;
+// I am constructing the strings from byte arrays to be slightly quicker than using StringBuilder.
 
 import static js.base.Tools.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class P22GenerateParenthesis {
@@ -18,40 +31,51 @@ public class P22GenerateParenthesis {
   }
 
   private void run() {
-    pr(generateParenthesis(3));
+    int n = 5;
+    var res = generateParenthesis(n);
+    pr(res);
+    pr("n:", n, "len:", res.size());
   }
 
   public List<String> generateParenthesis(int n) {
 
     List<String> results = new ArrayList<>();
 
-    var sb = new StringBuilder(n * 2);
-    int expected = 1 << (n-1);
-    for (int i = 0; i < expected; i++) {
-      int tailCount = 0;
-      sb.setLength(0);
-      var choices = i;
-      int j = 0;
-      while (j < n) {
-        if (tailCount != 0) {
-          boolean flag = (choices & 1) != 0;
-          choices >>= 1;
-          if (flag) {
-            tailCount--;
-            sb.append(')');
-            continue;
-          }
-        }
-        sb.append('(');
-        tailCount++;
-        j++;
-      }
-      while (tailCount-- != 0)
-        sb.append(')');
-      results.add(sb.toString());
+    int[] cursors = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      cursors[i] = i;
     }
+
+    byte[] str = new byte[n * 2];
+
+    outer: while (true) {
+      Arrays.fill(str, (byte) ')');
+      for (int c : cursors) {
+        str[c] = (byte) '(';
+      }
+      results.add(new String(str, StandardCharsets.US_ASCII));
+
+      int slot = n - 1;
+      while (true) {
+        // Increment current cursor.
+        // If reached limit, move back to the previous cursor and repeat.
+        cursors[slot]++;
+        if (cursors[slot] < 2 * slot + 1)
+          break;
+
+        slot--;
+        // If we've run out of cursors, we're done.
+        if (slot < 0)
+          break outer;
+      }
+
+      while (++slot < n) {
+        cursors[slot] = cursors[slot - 1] + 1;
+      }
+    }
+
     return results;
   }
-
 
 }
--- src/main/java/js/leetcode/P25ReverseKGroup.java
+++ src/main/java/js/leetcode/P25ReverseKGroup.java
@@ -9,6 +9,7 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
+import js.base.BasePrinter;
 import js.json.JSList;
 
 public class P25ReverseKGroup {
@@ -18,8 +19,11 @@ public class P25ReverseKGroup {
   }
 
   private void run() {
-    var a = buildNodes(1, 2, 3, 4, 5);
-    pr(reverseKGroup(a, 3));
+    int n = 24;
+    int k = 7;
+
+    var a = buildNodesN(n);
+    pr(reverseKGroup(a, k));
   }
 
   private static class ListNode {
@@ -51,6 +55,13 @@ public class P25ReverseKGroup {
     }
   }
 
+  private static ListNode buildNodesN(int length) {
+    int[] v = new int[length];
+    for (int i = 0; i < length; i++)
+      v[i] = i + 1;
+    return buildNodes(v);
+  }
+
   private static ListNode buildNodes(int... values) {
     ListNode head = null;
     ListNode tail = null;
@@ -67,35 +78,59 @@ public class P25ReverseKGroup {
   }
 
   public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode resultHead = null;
+    ListNode resultTail = null;
 
-    ListNode targetHandle = new ListNode();
-    ListNode targetTail = targetHandle;
-    int[] buffer = new int[k];
-    int bufferSize = 0;
+    while (head != null) {
+      // Try to construct a reversed k group
+      var chain = extractReversedChain(head, k);
 
-    while (true) {
-
-      bufferSize = 0;
-      for (int i = 0; i < k; i++) {
-        if (head == null) {
-          break;
-        }
-        buffer[bufferSize++] = head.val;
-        head = head.next;
+      if (chain.length < k) {
+        // Reverse the group back onto the end
+        chain = extractReversedChain(chain.head, chain.length);
       }
 
-      for (int j = 0; j < bufferSize; j++) {
-        int j2 = j;
-        if (bufferSize == k) {
-          j2 = k - 1 - j;
-        }
-        ListNode newTail = new ListNode(buffer[j2] );
-        targetTail.next = newTail;
-        targetTail = newTail;
+      if (resultHead == null) {
+        resultHead = chain.head;
+      } else {
+        resultTail.next = chain.head;
       }
-      if (bufferSize < k)
-        break;
+      resultTail = chain.tail;
+      head = chain.sourceHead;
     }
-    return targetHandle.next;
+
+    return resultHead;
   }
+
+  static class ReversedChain {
+    int length;
+    ListNode head;
+    ListNode tail;
+    ListNode sourceHead;
+
+    @Override
+    public String toString() {
+      return BasePrinter.toString("ReversedChain len", length, " head", head, "tail", tail, "sourceHead",
+          sourceHead);
+    }
+
+  }
+
+  // Extract and reverse a chain from a list. Returns a node whose value is the length of the chain, and
+  // whose next node is the first node in the reversed chain
+  //
+  private ReversedChain extractReversedChain(ListNode head, int chainSize) {
+    var r = new ReversedChain();
+
+    while (r.length < chainSize && head != null) {
+      r.head = new ListNode(head.val, r.head);
+      if (r.tail == null)
+        r.tail = r.head;
+      head = head.next;
+      r.length++;
+    }
+    r.sourceHead = head;
+    return r;
+  }
+
 }
