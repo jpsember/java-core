7f5e5e316177f93e793f55fec80ed23001c57e3e
Jeff Sember <jpsember@gmail.com>
2023-11-17 09:29:26 -0800

    Issue #23

--- src/main/java/js/leetcode/P32LongestValidParentheses.java
+++ src/main/java/js/leetcode/P32LongestValidParentheses.java
@@ -3,9 +3,12 @@ package js.leetcode;
 
 // 32. Longest Valid Parentheses
 //
+// Uses dynamic programming.  But there must be a better way as it is taking way too long. (Or I can optimize it somehow)
+//
 
 import static js.base.Tools.*;
-import java.util.BitSet;
+
+import java.util.ArrayList;
 
 public class P32LongestValidParentheses {
 
@@ -14,116 +17,47 @@ public class P32LongestValidParentheses {
   }
 
   private void run() {
-//    x(")()())");
+         x(")()())");
     
     // It takes a while, but it finds length 168 for this:
-    x("((())())(()))(()()(()(()))(()((((()))))))((()())()))()()(()(((((()()()())))()())(()()))((((((())))((()))()()))))(()))())))()))()())((()()))))(()(((((())))))()((()(()(())((((())(())((()()(()())))())(()(())()()))())(()()()))()(((()())(((()()())))(((()()()))(()()))()))()))))))())()()((()(())(()))()((()()()((())))()(((()())(()))())())))(((()))))())))()(())))()())))())()((()))((()))()))(((())((()()()(()((()((())))((()()))())(()()(()))))())((())))(()))()))))))()(()))())(()())))))(()))((())(()((())(((((()()()(()()())))(()())()((()(()()))(()(())((()((()))))))))(()(())()())()(()(()(()))()()()(()()())))(())(()((((()()))())))(())((()(())())))))())()()))(((())))())((()(()))(()()))((())(())))))(()(()((()((()()))))))(()()()(()()()(()(())()))()))(((()(())()())(()))())))(((()))())(()((()))(()((()()()(())()(()())()(())(()(()((((())()))(((()()(((()())(()()()(())()())())(()(()()((()))))()(()))))(((())))()()))(()))((()))))()()))))((((()(())()()()((()))((()))())())(()((()()())))))))()))(((()))))))(()())))(((()))((()))())))(((()(((())))())(()))))(((()(((((((((((((())(((()))((((())())()))())((((())(((())))())(((()))))()())()(())())(()))))()))()()()))(((((())()()((()))())(()))()()(()()))(())(()()))()))))(((())))))((()()(()()()()((())((((())())))))((((((()((()((())())(()((()))(()())())())(()(())(())(()((())((())))(())())))(()()())((((()))))((()(())(()(()())))))))))((()())()()))((()(((()((()))(((((()()()()()(()(()((()(()))(()(()((()()))))()(()()((((((()((()())()))((())()()(((((()(()))))()()((()())((()())()(())((()))()()(()))");
+    // x("((())())(()))(()()(()(()))(()((((()))))))((()())()))()()(()(((((()()()())))()())(()()))((((((())))((()))()()))))(()))())))()))()())((()()))))(()(((((())))))()((()(()(())((((())(())((()()(()())))())(()(())()()))())(()()()))()(((()())(((()()())))(((()()()))(()()))()))()))))))())()()((()(())(()))()((()()()((())))()(((()())(()))())())))(((()))))())))()(())))()())))())()((()))((()))()))(((())((()()()(()((()((())))((()()))())(()()(()))))())((())))(()))()))))))()(()))())(()())))))(()))((())(()((())(((((()()()(()()())))(()())()((()(()()))(()(())((()((()))))))))(()(())()())()(()(()(()))()()()(()()())))(())(()((((()()))())))(())((()(())())))))())()()))(((())))())((()(()))(()()))((())(())))))(()(()((()((()()))))))(()()()(()()()(()(())()))()))(((()(())()())(()))())))(((()))())(()((()))(()((()()()(())()(()())()(())(()(()((((())()))(((()()(((()())(()()()(())()())())(()(()()((()))))()(()))))(((())))()()))(()))((()))))()()))))((((()(())()()()((()))((()))())())(()((()()())))))))()))(((()))))))(()())))(((()))((()))())))(((()(((())))())(()))))(((()(((((((((((((())(((()))((((())())()))())((((())(((())))())(((()))))()())()(())())(()))))()))()()()))(((((())()()((()))())(()))()()(()()))(())(()()))()))))(((())))))((()()(()()()()((())((((())())))))((((((()((()((())())(()((()))(()())())())(()(())(())(()((())((())))(())())))(()()())((((()))))((()(())(()(()())))))))))((()())()()))((()(((()((()))(((((()()()()()(()(()((()(()))(()(()((()()))))()(()()((((((()((()())()))((())()()(((((()(()))))()()((()())((()())()(())((()))()()(()))");
+     x("(()");
   }
 
   private void x(String s) {
     pr(VERT_SP, "string:", s);
     int len = longestValidParentheses(s);
-    pr("len:", len);
-  }
-
-  private void dumpRow(int row, boolean withString) {
-    StringBuilder sb = new StringBuilder();
-
-    if (withString) {
-      sb.append("  ");
-      for (int i = 0; i < string.length(); i++) {
-        sb.append(i == 0 ? '|' : '|');
-        sb.append(" ");
-        sb.append(string.charAt(i));
-        sb.append(" ");
-      }
-      sb.append("|\n");
-    }
-    sb.append(row);
-    sb.append(" ");
-    for (int i = 0; i < string.length(); i++) {
-      sb.append(i == 0 ? '|' : ' ');
-      sb.append(" ");
-      if (valid(i, row))
-        sb.append('Y');
-      else
-        sb.append(' ');
-      sb.append(" ");
-    }
-    sb.append("|");
-    pr(sb.toString());
+    pr("valid length:", len);
   }
 
   public int longestValidParentheses(String s) {
-    stringLength = s.length();
-    string = s;
-    validFlags = new BitSet(stringLength * stringLength);
+    var stack = new ArrayList<Integer>();
 
-    int longest = 0;
+    int best = 0;
+    int validLength = 0;
 
-    for (int y = 1; y <= stringLength; y++) {
-
-     // pr(INDENT, "y:", y);
-      for (int x = 0; x < stringLength; x++) {
-       // pr("x:", x);
-        if (y >= 2) {
-          // Apply the wrap rule
-          if (valid(x + 1, y - 2) && charIs(x, '(') && charIs(x + y - 1, ')')) {
-            setValid(x, y);
-            longest = y;
-           // pr("...wrap rule applied");
-            continue;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      pr("i:",i,"char:",c,"valid:",validLength,"best:",best,"stack:",stack);
+      
+      if (c == '(') {
+        stack.add(validLength);
+        validLength = 0;
+      } else {
+        if (!stack.isEmpty()) {
+          validLength += stack.remove(stack.size() - 1) + 2; // add 2 for the opening and closing paren
+          pr("...valid now",validLength);
+          if (validLength > best) {
+            best = validLength;
+            pr("......new best");
           }
-        }
-
-        // Apply the concatenate rule
-        for (int k = 2; k <= y - 2; k++) {
-          // If there are adjacent valid substrings
-          // of length [k] [y-k] (positioned at x-k, k and x, y-k respectively)
-          //
-          // then the substring [y] at x-k is valid.
-
-          if (// 
-          valid(x - k, k) //
-              && valid(x, y - k)) {
-            setValid(x-k, y);
-            longest = y;
-           // pr("...concatenate rule applied");
-          }
+        } else {
+          validLength = 0;
+          pr("...reset valid");
         }
       }
-
-//      for (int i = 1; i <= y; i++)
-//        dumpRow(i, i == 1);
     }
-    return longest;
+    return best;
   }
 
-
-  private int bitIndex(int x, int y) {
-    return (y - 1) * stringLength + x;
-  }
-
-  private boolean valid(int x, int y) {
-    if (x < 0 || x >= stringLength)
-      return false;
-    if (y == 0)
-      return true;
-    return validFlags.get(bitIndex(x, y));
-  }
-
-  private void setValid(int x, int y) {
-    validFlags.set(bitIndex(x, y));
-  }
-
-  private boolean charIs(int pos, char value) {
-    if (pos < 0 || pos >= string.length())
-      return false;
-    return string.charAt(pos) == value;
-  }
-
-  // These instance fields would be refactored in 'real' code
-  private BitSet validFlags;
-  private String string;
-  private int stringLength;
 }
