40b82141021698cd707a83187f139772e4bf0e0b
Jeff Sember <jpsember@gmail.com>
2023-11-19 14:46:08 -0800

    Issue #23

--- src/main/java/js/leetcode/P51NQueens.java
+++ src/main/java/js/leetcode/P51NQueens.java
@@ -10,9 +10,8 @@ import java.util.List;
 // Runtime: Beats 23.72% of users with Java
 // Memory:  Beats 5.3%
 //
-// Added reflection heuristic:
-// Runtime: Beats 18.42% of users with Java
-// Memory:  Beats 9.28%of users with Java
+// Slightly better performance in other algorithms using recursive calls and an explicit board array
+//
 
 public class P51NQueens {
 
@@ -41,22 +40,23 @@ public class P51NQueens {
     }
   }
 
+  // Beats 62% runtime, 98.55% memory
   public List<List<String>> solveNQueens(int n) {
 
-    List<List<String>> result = new ArrayList<>(352);
+    List<List<String>> stringResults = new ArrayList<>(352);
 
     // Coordinates of queen we are attempting to place
     int bx = 0;
     int by = 0;
 
-    int nMid = (n + 1) / 2;
-
     // Columns of queens already placed
     int[] queenSlots = new int[n];
+    String[] queenRowStrings = new String[n];
+    for (int i = 0; i < n; i++) {
+      queenRowStrings[i] = "........Q........".substring(8 - i, 8 - i + n);
+    }
 
-    StringBuilder sb = new StringBuilder();
-
-    while (true) {
+    outer: while (true) {
 
       // Are we past the last row, or is the current row exhausted?
       if (by == n || bx == n) {
@@ -66,87 +66,37 @@ public class P51NQueens {
         by--;
         // Advance to the next column
         bx = queenSlots[by] + 1;
-
         continue;
       }
 
       // Is this an invalid move?
       {
-        boolean valid = true;
         for (int qy = 0; qy < by; qy++) {
           int qx = queenSlots[qy];
-          int dx = Math.abs(qx - bx);
-          int dy = Math.abs(qy - by);
-          if (dx == 0 || dy == 0 || dx == dy) {
-            valid = false;
-            break;
+          int dx = qx - bx;
+          int dy = qy - by;
+          if (dx == 0  || dx == dy || dx == -dy) {
+            bx++;
+            continue outer;
           }
-        }
-        if (!valid) {
-          bx++;
-          continue;
         }
       }
 
       // Place queen, move to next row
-      queenSlots[by] = (byte) bx;
+      queenSlots[by] = bx;
       by++;
       bx = 0;
 
       // If all n queens have been placed, it is a solution
       if (by == n) {
-
-        // Special case: if we've generated a solution where the queen is past the midpoint of the first row,
-        // stop and use symmetry to generate other solutions.
-        if (queenSlots[0] >= nMid) {
-          break;
+        List<String> strings = new ArrayList<>(n);
+        for (int k : queenSlots) { 
+          strings.add(queenRowStrings[k]);
         }
-
-        List<String> soln = new ArrayList<>(n);
-        for (int y = 0; y < n; y++) {
-          sb.setLength(0);
-          for (int x = 0; x < n; x++) {
-            if (x == queenSlots[y])
-              sb.append('Q');
-            else
-              sb.append('.');
-          }
-          soln.add(sb.toString());
-        }
-
-        result.add(soln);
-
+        stringResults.add(strings);
       }
     }
-
-    // Use symmetry to generate other solutions
-    if (!result.isEmpty()) {
-
-      int j = result.size();
-
-      var match = "";
-      if ((n & 1) != 0) {
-        var lastGenerated = result.get(j - 1);
-        match = lastGenerated.get(0);
-      }
-
-      for (int i = 0; i < j; i++) {
-        var orig = result.get(i);
-        // Don't generate reflected copies of a solution whose
-        // first row queen is in the central column, as we have already generated them
-        if (orig.get(0).equals(match))
-          break;
-        List<String> soln = new ArrayList<>(n);
-        for (var s : orig) {
-          sb.setLength(0);
-          for (int k = n - 1; k >= 0; k--)
-            sb.append(s.charAt(k));
-          soln.add(sb.toString());
-        }
-        result.add(soln);
-      }
-    }
-
-    return result;
+    return stringResults;
   }
+
 }
