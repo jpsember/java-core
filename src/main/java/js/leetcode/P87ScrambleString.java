54054ae4b13a375e97b9d499485dc04b4fb6487f
Jeff Sember <jpsember@gmail.com>
2023-11-20 12:16:06 -0800

    Issue #23: time limit exceeded on algorithm that works on duplicates

--- src/main/java/js/leetcode/P87ScrambleString.java
+++ src/main/java/js/leetcode/P87ScrambleString.java
@@ -2,7 +2,6 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,18 +14,24 @@ public class P87ScrambleString {
   }
 
   private void run() {
-    go2("great", "rgeat");
-    go2("abcde", "caebd");
-    go2("a", "a");
-    // This doesn't work, since there are duplicates
-    go2("abcdbdacbdac","bdacabcdbdac");
+//    go2("abc", "acb");
+//    go2("great", "rgeat");
+//    go2("abcde", "caebd");
+//    go2("a", "a");
+    // This doesn't work with my original algorithm, since there are duplicates
+     go2("abcdbdacbdac", "bdacabcdbdac");
+     
+    go2("ab","aa");
   }
 
   private void go2(String orig, String scr) {
-    pr(orig, CR, scr, INDENT, isScramble(orig, scr));
+    pr(CR, orig, CR, scr, isScramble(orig, scr));
   }
 
   public boolean isScramble(String s1, String s2) {
+    // If there are duplicate characters, use the second algorithm
+    if (hasDuplicateChars(s1) || hasDuplicateChars(s2))
+      return isScramble2(s1, s2);
     // Define a mapping over the characters to convert them to their position within the string
     Map<Character, Integer> m = new HashMap<>();
     int[] dig1 = new int[s1.length()];
@@ -41,11 +46,16 @@ public class P87ScrambleString {
         return false;
       dig2[i] = j;
     }
-
-    //    pr("dig1:", dig1);
-    //    pr("dig2:", dig2);
-
     return auxScramble(dig2, 0, dig2.length);
+  }
+
+  private static boolean hasDuplicateChars(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (s.substring(i + 1).indexOf(c) >= 0)
+        return true;
+    }
+    return false;
   }
 
   private boolean less(MinMax a, MinMax b, boolean fwd) {
@@ -125,4 +135,65 @@ public class P87ScrambleString {
     }
 
   }
+
+  private Map<String, String> sortedMap = new HashMap<>();
+  private StringBuilder sb = new StringBuilder();
+
+  private String sorted(String s) {
+    String result = sortedMap.get(s);
+    if (result == null) {
+      sb.setLength(0);
+      sb.append(s);
+      for (int i = 0; i < s.length(); i++) {
+        char ci = sb.charAt(i);
+        for (int j = 0; j < s.length(); j++) {
+          char cj = sb.charAt(j);
+          if (ci > cj) {
+            sb.setCharAt(j, ci);
+            sb.setCharAt(i, cj);
+            var tmp = ci;
+            ci = cj;
+            cj = tmp;
+          }
+        }
+      }
+      result = sb.toString();
+      sortedMap.put(s, result);
+    }
+    return result;
+  }
+
+  private boolean isScramble2(String s1, String s2) {
+    
+    if (!sorted(s1).equals(sorted(s2))) {
+      return false;
+    }
+
+    if (s1.length() == 1)
+      return true;
+
+    for (int i = 1; i < s1.length(); i++) {
+
+      // Divide both a and b at i
+      var s1a = s1.substring(0, i);
+      var s1b = s1.substring(i);
+
+      var s2a = s2.substring(0, i);
+      var s2b = s2.substring(i);
+
+      if (isScramble2(s1a, s2a) && isScramble(s1b, s2b))
+        return true;
+
+      // Simulate effect of swapping at i
+      int j = s1.length() - i;
+      var s3a = s2.substring(j);
+      var s3b = s2.substring(0, j);
+
+      if (isScramble2(s1a, s3a) && isScramble(s1b, s3b))
+        return true;
+    }
+
+    return false;
+  }
+
 }
