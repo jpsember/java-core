ddaa9dcc89c330f4372ab6c5a90e08a0379f3b81
Jeff Sember <jpsember@gmail.com>
2023-11-20 18:50:03 -0800

    Issue #23

--- src/main/java/js/leetcode/P84LargestRectangleInHistogram.java
+++ src/main/java/js/leetcode/P84LargestRectangleInHistogram.java
@@ -19,47 +19,11 @@ public class P84LargestRectangleInHistogram {
     pr(heights, "answer:", answer);
   }
 
-  private void dump(Column c, String prompt) {
-    Column head = c;
-    while (head.prev != null)
-      head = head.prev;
-
-    pr(VERT_SP, prompt);
-    StringBuilder sb = new StringBuilder();
-
-    Column w = head;
-    while (w != null) {
-      if (w == c)
-        sb.append(">");
-      else
-        sb.append(" ");
-
-      sb.append(" #" + w.id + " " + w.height);
-
-      sb.append("|");
-      for (int x = 0; x < w.width; x++) {
-        sb.append(":::: ");
-      }
-      sb.append("|");
-      w = w.next;
-    }
-    pr(sb.toString());
-  }
-
-  static int uniqueId = 50;
-
   private static class Column {
     int height;
     int width;
     Column prev;
     Column next;
-    int id = uniqueId++;
-
-    @Override
-    public String toString() {
-      return "#" + id + " ht:" + height + " wd:" + width;
-    }
-
   }
 
   public int largestRectangleArea(int[] heights) {
@@ -88,10 +52,9 @@ public class P84LargestRectangleInHistogram {
 
     int maxArea = 0;
 
-    Column c = head; //.next;
+    Column c = head;
 
     while (true) {
-      dump(c, "iteration");
       var cNext = c.next;
       if (cNext == null)
         break;
@@ -100,31 +63,26 @@ public class P84LargestRectangleInHistogram {
       if (c == head && cNext == tail)
         break;
 
-      pr("c:", c, "head:", head, "cNext:", cNext, "tail:", tail);
-      
-      if (cNext.height > c.height ) {
+      if (cNext.height > c.height) {
         c = cNext;
         continue;
       }
-      
+
       // If current is head, next is also zero; delete the next
       if (c == head) {
-        join(c,cNext.next);
+        join(c, cNext.next);
         continue;
       }
-      
+
       // Next height is lower than current.
       // Calculate area of rectangle positioned at top of current as candidate,
       // then truncate it and add it to its predecessor.
 
       int area = c.height * c.width;
       maxArea = Math.max(area, maxArea);
-      pr("next height is lower; area of previous block:", area, "newmax:", maxArea);
 
       // Extend predecessor by current's width
       Column cp = c.prev;
-
-      pr("c prev:", cp);
 
       // Not strictly necessary, but avoid modifying original head (its height is zero so it doesn't matter what width it has)
       if (cp != head) {
@@ -134,7 +92,6 @@ public class P84LargestRectangleInHistogram {
       // delete current
       c = cp;
       join(cp, cNext);
-      dump(c, "deleted current");
     }
     return maxArea;
   }
