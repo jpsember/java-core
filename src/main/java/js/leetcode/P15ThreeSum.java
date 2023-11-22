bb9383d7ba9f871bd54a899e3614ee1a12f9c16b
Jeff Sember <jpsember@gmail.com>
2023-11-22 11:51:04 -0800

    Issue #23

--- src/main/java/js/leetcode/P15ThreeSum.java
+++ src/main/java/js/leetcode/P15ThreeSum.java
@@ -37,7 +37,7 @@ public class P15ThreeSum {
 
     //x(-1, 0, 1, 0);
 
-    if (true) {
+    if (false) {
       int y = 20;
       int[] nums = new int[2 * y + 3];
       int i = 3;
@@ -48,7 +48,7 @@ public class P15ThreeSum {
       x(nums);
     }
 
-    if (true) {
+    if (false) {
       var r = new Random(1965);
       int[] nums = new int[1000];
 
@@ -63,6 +63,8 @@ public class P15ThreeSum {
 
   private void x(int... nums) {
     var verified = sorted(slow(nums));
+    pr(CR);
+    
     var result = sorted(threeSum(nums));
     /**/pr(CR, result, CR, "count:", result.size());
     if (!result.equals(verified)) {
@@ -106,66 +108,110 @@ public class P15ThreeSum {
     return new ArrayList<>(result.values());
   }
 
+  private int c3;
+
   public List<List<Integer>> threeSum(int[] nums) {
     result.clear();
     Arrays.sort(nums);
 
-    int a = 0;
-    int b = nums.length - 1;
+    // Find c, the minimum value
+    int c = nums[0];
+    for (int x : nums)
+      if (x < c)
+        c = x;
+    c = -c;
+    c3 = c * 3;
 
-    while (a < b) {
-      int va = nums[a];
-      int vb = nums[b];
-      if ((va ^ vb) >= 0)
-        break;
+    int[] u = new int[nums.length];
+    int[] w = new int[nums.length];
 
-      for (var a1 = a; a1 + 1 < b; a1++) {
-        int va1 = nums[a1];
-        int vc = 0 - (va1 + vb);
-        int k = Arrays.binarySearch(nums, a1 + 1, b, vc);
-        if (k >= 0) {
-          addSoln(va1, vb, vc);
-        } else {
-          // Short circuit: 
-          // If the insertion position for this missing value is to the left, we won't find any more results 
-          // stepping further to the right.
-          if (-k - 1 <= a1)
-            break;
+    int belowC = 0;
+    int equalC = 0;
+    int midC = 0;
+    {
+      for (int x : nums) {
+        var p = x + c;
+        if (p < c) {
+          u[belowC++] = p;
+        } else if (p == c)
+          equalC++;
+        else if (p <= c3) {
+          w[midC++] = p;
         }
       }
-
-      for (var b1 = b - 1; b1 - 1 > a; b1--) {
-        int vb1 = nums[b1];
-        int vc = 0 - (va + vb1);
-        int k = Arrays.binarySearch(nums, a + 1, b1, vc);
-        if (k >= 0) {
-          addSoln(va, vb1, vc);
-        } else {
-          // Short circuit: 
-          // If the insertion position for this missing value is to the right, we won't find any more results 
-          // stepping further to the left.
-          if (-k - 1 >= b1)
-            break;
-        }
-      }
-      a++;
-      b--;
+    }
+    if (equalC >= 3) {
+      addSoln(0, 0, 0);
     }
 
-    // Add 0,0,0 if there are at least three zeros
-    int k = Arrays.binarySearch(nums, 0);
-    if (k >= 0) {
-      while (k > 0 && nums[k - 1] == 0)
-        k--;
-      if (k + 2 < nums.length && nums[k + 1] == 0 && nums[k + 2] == 0) {
-        addSoln(0, 0, 0);
+    pr("nums:",nums);
+    pr("c:", c, "3c:", c3, "u:", u, "w:", w,"below:",belowC,"midC:",midC,"equal:",equalC);
+
+    var umap = buildFreqMap(u, belowC);
+    var wmap = buildFreqMap(w, midC);
+    pr("umap:", umap);
+    pr("wmap:", wmap);
+    
+    
+    for (int i = 0; i < belowC; i++) {
+      var uVal = u[i];
+    pr("u["+i+"]: ",uVal);
+    
+      {
+        var wVal = 2 * c - uVal;
+        pr("w:",wVal);
+        if (wmap.containsKey(wVal)) {
+          addSoln(uVal - c, c, wVal - c);
+        }
       }
+      var ww = findTwoSum(w, wmap, c3 - uVal);
+      if (ww != null)
+        addSoln(uVal - c, ww[0] - c, ww[1] - c);
+    }
+
+    for (int i = 0; i < midC; i++) {
+      var wVal = w[i];
+      var uu = findTwoSum(u, umap, c3 - wVal);
+      if (uu != null)
+        addSoln(uu[0] - c, uu[1] - c, wVal - c);
     }
 
     return new ArrayList<>(result.values());
   }
 
+  private static int[] work = new int[2];
+
+  private int[] findTwoSum(int[] list, Map<Integer, Integer> freq, int sum) {
+    int size = freq.size();
+    for (int i = 0; i < size; i++) {
+      int a = list[i];
+      int b = sum - a;
+      Integer count = freq.get(b);
+      if (count != null) {
+        if (a == b && count < 2)
+          break;
+        work[0] = a;
+        work[1] = b;
+        return work;
+      }
+    }
+    return null;
+  }
+
+  private Map<Integer, Integer> buildFreqMap(int[] a, int len) {
+    var res = new HashMap<Integer, Integer>(len);
+    for (int i = 0; i < len; i++) {
+      var x = a[i];
+      Integer count = res.get(x);
+      if (count == null)
+        count = 0;
+      res.put(x, count + 1);
+    }
+    return res;
+  }
+
   private void addSoln(int a, int b, int c) {
+    pr("===>",a,b,c);
     final long minNum = -100000;
     long key = (a - minNum) + ((b - minNum) << 17) + ((c - minNum) << (17 * 2));
     List<Integer> soln = new ArrayList<>(3);
