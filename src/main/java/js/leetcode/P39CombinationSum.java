084d4f977932484fd0c286eaf72cb76b7cc6cda9
Jeff Sember <jpsember@gmail.com>
2023-11-23 19:48:29 -0800

    Issue #23

--- src/main/java/js/leetcode/P39CombinationSum.java
+++ src/main/java/js/leetcode/P39CombinationSum.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import js.data.DataUtil;
-import js.json.JSList;
 
 // I thought at first that I could use linear algebra, but a) the system of linear equations
 // is under constrained, so the matrix is invertible; and b) we are only interested in INTEGER
@@ -20,6 +19,9 @@ import js.json.JSList;
 // I realize now that this is the integer knapsack problem.
 //
 // First attempt: runs slower than 95% of the accepted solutions.
+//
+// Second attempt: uses recursive approach, where each iteration handles a particular 'coin value';
+// now in 99th percentile
 //
 public class P39CombinationSum {
 
@@ -37,87 +39,46 @@ public class P39CombinationSum {
     pr(result);
   }
 
-  public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    int numCandidates = candidates.length;
-
-    State states[] = new State[1 + target];
-    for (int i = 0; i <= target; i++)
-      states[i] = new State();
-    states[0].addCombination(new int[numCandidates]);
-
-    long frontier = 1;
-    while (frontier != 0) {
-
-      pr(VERT_SP, "next iteration; stack:", DataUtil.bitString((int) (frontier >> 32)),
-          DataUtil.bitString((int) frontier));
-      long nextFrontier = 0;
-
-      for (var stateNumber = 0; stateNumber < 40; stateNumber++, frontier >>= 1) {
-        if ((frontier & 1) == 0)
-          continue;
-
-        var s = states[stateNumber];
-
-        for (var c : s.combinations) {
-          loop: for (int j = 0; j < numCandidates; j++) {
-            // If this combination already has higher-valued values, skip this value
-            for (int k = j + 1; k < numCandidates; k++)
-              if (c[k] > 0)
-                continue loop;
-            int newSum = stateNumber + candidates[j];
-            if (newSum > target)
-              continue;
-            State nextState = states[newSum];
-            int[] newCombination = Arrays.copyOf(c, c.length);
-            newCombination[j]++;
-            if (nextState.addCombination(newCombination))
-              nextFrontier |= 1L << newSum;
-          }
-        }
-      }
-      frontier = nextFrontier;
-    }
-
-    var targetState = states[target];
-
-    List<List<Integer>> resultList = new ArrayList<>();
-
-    for (var combination : targetState.combinations) {
-      List<Integer> result = new ArrayList<>();
-      int i = 0;
-      for (var count : combination) {
-        var value = candidates[i];
-        for (int k = 0; k < count; k++)
-          result.add(value);
-        i++;
-      }
-      resultList.add(result);
-    }
+  
+  public List<List<Integer>> combinationSum(int[] coinValues, int target) {
+    //pr("combinationSum, coinValues:", coinValues, "target:", target);
+    resultList = new ArrayList<>();
+    mCoinValues = coinValues;
+    mCoinValueCount = coinValues.length;
+    int[] counts = new int[mCoinValueCount];
+    aux(counts, target, 0);
     return resultList;
   }
 
-  private static class State {
-    List<int[]> combinations = new ArrayList<>();
-
-    boolean addCombination(int[] combo) {
-      // If combination is already in the state, do nothing
-      for (var c : combinations) {
-        if (Arrays.equals(c, combo))
-          return false;
+  private void aux(int[] coinCounts, int target, int coinCountStart) {
+    //pr(VERT_SP, "target:", target, "coinVals:", mCoinValues, "counts:", coinCounts, "start:", coinCountStart);
+    if (coinCountStart == mCoinValueCount) {
+      if (target == 0) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < mCoinValueCount; i++) {
+          var coinValue = mCoinValues[i];
+          int count = coinCounts[i];
+          for (int j = 0; j < count; j++)
+            result.add(coinValue);
+        }
+        resultList.add(result);
       }
-      combinations.add(combo);
-      return true;
+      return;
     }
-
-    //    @Override
-    //    public String toString() {
-    //      StringBuilder sb = new StringBuilder();
-    //      for (var c : combinations) {
-    //        sb.append(' ');
-    //        sb.append(JSList.with(c).toString());
-    //      }
-    //      return sb.toString();
-    //    }
+    int c = mCoinValues[coinCountStart];
+    //pr("coinValue #" + coinCountStart + ":", c);
+    int saveCount = coinCounts[coinCountStart];
+    var newTarget = target;
+    while (newTarget >= 0) {
+      aux(coinCounts, newTarget, coinCountStart + 1);
+      coinCounts[coinCountStart]++;
+      newTarget -= c;
+    }
+    coinCounts[coinCountStart] = saveCount;
   }
+
+  private int mCoinValueCount;
+  private List<List<Integer>> resultList;
+  private int[] mCoinValues;
 
 }
