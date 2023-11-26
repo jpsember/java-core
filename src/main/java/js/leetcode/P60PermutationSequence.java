4b6e876d336047ecb97c66905fe17f352749b63d
Jeff Sember <jpsember@gmail.com>
2023-11-26 09:59:15 -0800

    Issue #23

--- src/main/java/js/leetcode/P60PermutationSequence.java
+++ src/main/java/js/leetcode/P60PermutationSequence.java
@@ -3,6 +3,10 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
+/**
+ * I exploit a relationship between the digits of a permutation and the
+ * factorial function to perform a series of swaps of the digits 1234...
+ */
 public class P60PermutationSequence {
 
   public static void main(String[] args) {
@@ -11,6 +15,8 @@ public class P60PermutationSequence {
 
   private void run() {
     //  x(4,1,"1234");
+    x(3, 5, "312");
+    x(9, 1, "123456789");
     x(4, 1, "1234");
 
     x(3, 3, "213");
@@ -26,38 +32,30 @@ public class P60PermutationSequence {
   }
 
   public String getPermutation(int n, int k) {
+    pr("n:", n, "k:", k);
+    int[] factorials = new int[n]; // table of factorial(i+1)
+    int f = 1;
+    for (int i = 1; i <= n; i++) {
+        f *= i;
+      factorials[i - 1] = f;
+    }
+    pr("factorials:", factorials);
     var sb = new StringBuilder("123456789".substring(0, n));
     for (int digit = n; digit > 1; digit--) {
       // In calculations, work with k (0..n-1), not (1..n)
-      var a = (k - 1) % fact(digit);
-      var b = fact(digit - 1);
+      var a = (k - 1) % factorials[digit - 1];
+      var b = factorials[digit - 2];
+      pr("digit:", digit, "a:", a, "b:", b, "sb:", sb);
       if (a >= b) {
         int i = n - digit;
         int j = i + 1;
         var tmp = sb.charAt(i);
         sb.setCharAt(i, sb.charAt(j));
         sb.setCharAt(j, tmp);
+        pr("...after swap", i, j, ":", sb);
       }
     }
     return sb.toString();
   }
-
-  private static long fact(int n) {
-    if (factorials == null) {
-      final int maxN = 9;
-      factorials = new long[maxN + 1];
-      {
-        long f = 1;
-        for (int i = 0; i <= maxN; i++) {
-          if (i > 0)
-            f *= i;
-          factorials[i] = f;
-        }
-      }
-    }
-    return factorials[n];
-  }
-
-  private static long[] factorials;
 
 }
