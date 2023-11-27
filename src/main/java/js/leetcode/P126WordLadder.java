7e01aef8889819c1dce26ede0a2a292985d0ff5a
Jeff Sember <jpsember@gmail.com>
2023-11-26 16:06:52 -0800

    Issue #23

--- src/main/java/js/leetcode/P126WordLadder.java
+++ src/main/java/js/leetcode/P126WordLadder.java
@@ -4,6 +4,7 @@ package js.leetcode;
 import static js.base.Tools.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,26 +19,94 @@ public class P126WordLadder {
   }
 
   private void run() {
-    x("hit cog hot dot dog lot log cog", 2);
+    //    x("hit cog hot dot dog lot log cog", 2);
+    //    x("hit cog hot dot dog lot log", 0);
+    //    x("a b c d e f b g h", 1);
+
+    y("abc","def","abd","abe","abf","abg","aec","aef","def");
+    if (false)
+    y("aaaaa", "ggggg", //
+        "aaaaa", "caaaa", "cbaaa", "daaaa", "dbaaa", "eaaaa", "ebaaa", "faaaa", "fbaaa", "gaaaa", "gbaaa",
+        "haaaa", "hbaaa", "iaaaa", "ibaaa", "jaaaa", "jbaaa", "kaaaa", "kbaaa", "laaaa", "lbaaa", "maaaa",
+        "mbaaa", "naaaa", "nbaaa", "oaaaa", "obaaa", "paaaa", "pbaaa", "bbaaa", "bbcaa", "bbcba", "bbdaa",
+        "bbdba", "bbeaa", "bbeba", "bbfaa", "bbfba", "bbgaa", "bbgba", "bbhaa", "bbhba", "bbiaa", "bbiba",
+        "bbjaa", "bbjba", "bbkaa", "bbkba", "bblaa", "bblba", "bbmaa", "bbmba", "bbnaa", "bbnba", "bboaa",
+        "bboba", "bbpaa", "bbpba", "bbbba", "abbba", "acbba", "dbbba", "dcbba", "ebbba", "ecbba", "fbbba",
+        "fcbba", "gbbba", "gcbba", "hbbba", "hcbba", "ibbba", "icbba", "jbbba", "jcbba", "kbbba", "kcbba",
+        "lbbba", "lcbba", "mbbba", "mcbba", "nbbba", "ncbba", "obbba", "ocbba", "pbbba", "pcbba", "ccbba",
+        "ccaba", "ccaca", "ccdba", "ccdca", "cceba", "cceca", "ccfba", "ccfca", "ccgba", "ccgca", "cchba",
+        "cchca", "cciba", "ccica", "ccjba", "ccjca", "cckba", "cckca", "cclba", "cclca", "ccmba", "ccmca",
+        "ccnba", "ccnca", "ccoba", "ccoca", "ccpba", "ccpca", "cccca", "accca", "adcca", "bccca", "bdcca",
+        "eccca", "edcca", "fccca", "fdcca", "gccca", "gdcca", "hccca", "hdcca", "iccca", "idcca", "jccca",
+        "jdcca", "kccca", "kdcca", "lccca", "ldcca", "mccca", "mdcca", "nccca", "ndcca", "occca", "odcca",
+        "pccca", "pdcca", "ddcca", "ddaca", "ddada", "ddbca", "ddbda", "ddeca", "ddeda", "ddfca", "ddfda",
+        "ddgca", "ddgda", "ddhca", "ddhda", "ddica", "ddida", "ddjca", "ddjda", "ddkca", "ddkda", "ddlca",
+        "ddlda", "ddmca", "ddmda", "ddnca", "ddnda", "ddoca", "ddoda", "ddpca", "ddpda", "dddda", "addda",
+        "aedda", "bddda", "bedda", "cddda", "cedda", "fddda", "fedda", "gddda", "gedda", "hddda", "hedda",
+        "iddda", "iedda", "jddda", "jedda", "kddda", "kedda", "lddda", "ledda", "mddda", "medda", "nddda",
+        "nedda", "oddda", "oedda", "pddda", "pedda", "eedda", "eeada", "eeaea", "eebda", "eebea", "eecda",
+        "eecea", "eefda", "eefea", "eegda", "eegea", "eehda", "eehea", "eeida", "eeiea", "eejda", "eejea",
+        "eekda", "eekea", "eelda", "eelea", "eemda", "eemea", "eenda", "eenea", "eeoda", "eeoea", "eepda",
+        "eepea", "eeeea", "ggggg", "agggg", "ahggg", "bgggg", "bhggg", "cgggg", "chggg", "dgggg", "dhggg",
+        "egggg", "ehggg", "fgggg", "fhggg", "igggg", "ihggg", "jgggg", "jhggg", "kgggg", "khggg", "lgggg",
+        "lhggg", "mgggg", "mhggg", "ngggg", "nhggg", "ogggg", "ohggg", "pgggg", "phggg", "hhggg", "hhagg",
+        "hhahg", "hhbgg", "hhbhg", "hhcgg", "hhchg", "hhdgg", "hhdhg", "hhegg", "hhehg", "hhfgg", "hhfhg",
+        "hhigg", "hhihg", "hhjgg", "hhjhg", "hhkgg", "hhkhg", "hhlgg", "hhlhg", "hhmgg", "hhmhg", "hhngg",
+        "hhnhg", "hhogg", "hhohg", "hhpgg", "hhphg", "hhhhg", "ahhhg", "aihhg", "bhhhg", "bihhg", "chhhg",
+        "cihhg", "dhhhg", "dihhg", "ehhhg", "eihhg", "fhhhg", "fihhg", "ghhhg", "gihhg", "jhhhg", "jihhg",
+        "khhhg", "kihhg", "lhhhg", "lihhg", "mhhhg", "mihhg", "nhhhg", "nihhg", "ohhhg", "oihhg", "phhhg",
+        "pihhg", "iihhg", "iiahg", "iiaig", "iibhg", "iibig", "iichg", "iicig", "iidhg", "iidig", "iiehg",
+        "iieig", "iifhg", "iifig", "iighg", "iigig", "iijhg", "iijig", "iikhg", "iikig", "iilhg", "iilig",
+        "iimhg", "iimig", "iinhg", "iinig", "iiohg", "iioig", "iiphg", "iipig", "iiiig", "aiiig", "ajiig",
+        "biiig", "bjiig", "ciiig", "cjiig", "diiig", "djiig", "eiiig", "ejiig", "fiiig", "fjiig", "giiig",
+        "gjiig", "hiiig", "hjiig", "kiiig", "kjiig", "liiig", "ljiig", "miiig", "mjiig", "niiig", "njiig",
+        "oiiig", "ojiig", "piiig", "pjiig", "jjiig", "jjaig", "jjajg", "jjbig", "jjbjg", "jjcig", "jjcjg",
+        "jjdig", "jjdjg", "jjeig", "jjejg", "jjfig", "jjfjg", "jjgig", "jjgjg", "jjhig", "jjhjg", "jjkig",
+        "jjkjg", "jjlig", "jjljg", "jjmig", "jjmjg", "jjnig", "jjnjg", "jjoig", "jjojg", "jjpig", "jjpjg",
+        "jjjjg", "ajjjg", "akjjg", "bjjjg", "bkjjg", "cjjjg", "ckjjg", "djjjg", "dkjjg", "ejjjg", "ekjjg",
+        "fjjjg", "fkjjg", "gjjjg", "gkjjg", "hjjjg", "hkjjg", "ijjjg", "ikjjg", "ljjjg", "lkjjg", "mjjjg",
+        "mkjjg", "njjjg", "nkjjg", "ojjjg", "okjjg", "pjjjg", "pkjjg", "kkjjg", "kkajg", "kkakg", "kkbjg",
+        "kkbkg", "kkcjg", "kkckg", "kkdjg", "kkdkg", "kkejg", "kkekg", "kkfjg", "kkfkg", "kkgjg", "kkgkg",
+        "kkhjg", "kkhkg", "kkijg", "kkikg", "kkljg", "kklkg", "kkmjg", "kkmkg", "kknjg", "kknkg", "kkojg",
+        "kkokg", "kkpjg", "kkpkg", "kkkkg", "ggggx", "gggxx", "ggxxx", "gxxxx", "xxxxx", "xxxxy", "xxxyy",
+        "xxyyy", "xyyyy", "yyyyy", "yyyyw", "yyyww", "yywww", "ywwww", "wwwww", "wwvww", "wvvww", "vvvww",
+        "vvvwz", "avvwz", "aavwz", "aaawz", "aaaaz");
 
   }
 
-  private void x(String s, int expectedCount) {
+  private void y(String begin, String end, String... words) {
+    // List<String> wd = new ArrayList<>();
+    //    for (var x : words)
+    //      wd.add(x);
+    var ls = yAux(begin, end, new ArrayList<>(Arrays.asList(words)));
+    pr(ls);
+    //  checkState(ls.size() == expectedCount, "expected:", expectedCount, "but got:", ls.size());
+  }
+
+  private JSList yAux(String begin, String end, List<String> wd) {
+    //    List<String> wd = new ArrayList<>();
+    //    for (var x : words)
+    //      wd.add(x);
+    var result = findLadders(begin, end, wd);
+    var ls = list();
+    for (var x : result) {
+      ls.add(JSList.withStringRepresentationsOf(x));
+    }
+    return ls;
+  }
+
+  /* private */ void x(String s, int expectedCount) {
     var ws = split(s, ' ');
     int i = 0;
     var beginWord = ws.get(i++);
     var endWord = ws.get(i++);
     var wordList = ws.subList(i, ws.size());
-    var result = findLadders(beginWord, endWord, wordList);
-    var ls = list();
-    for (var x : result) {
-      ls.add(JSList.withStringRepresentationsOf(x));
-    }
-    pr(ls);
-    checkState(ls.size() == expectedCount, "expected:", expectedCount, "but got:", ls.size());
+    var result = yAux(beginWord, endWord, wordList);
+    pr(result);
+    checkState(result.size() == expectedCount, "expected:", expectedCount, "but got:", result.size());
   }
 
-  private void dumpGraph(String prompt) {
+  /* private */ void dumpGraph(String prompt) {
     JSMap m = map();
     for (var n : mGraph.values()) {
       var lst = list();
@@ -50,22 +119,61 @@ public class P126WordLadder {
   }
 
   public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-    List<List<String>> results = new ArrayList<>();
+    results.clear();
     wordLength = beginWord.length();
     wordList.add(beginWord);
-    // wordList.add(endWord);
     constructGraph(wordList);
     wordList.remove(wordList.size() - 1);
 
     beginNode = mGraph.get(beginWord);
     endNode = mGraph.get(endWord);
-    if (endNode != null) {
+
+    {
       doBFS();
-      dumpGraph("after BFS");
+      //dumpGraph("after BFS");
       filterEdges();
-      dumpGraph("after filter");
+      if (withDeadEndFilter) {
+        auxFilterDeadEnd(beginNode);
+        //dumpGraph("after filter");
+        // halt();
+      }
+      findResults(beginNode);
     }
     return results;
+  }
+
+  int depth;
+
+  private void findResults(Node node) {
+    if (node == endNode) {
+      checkState(node.links.isEmpty());
+      List<String> solution = new ArrayList<>(path.size());
+      for (var n : path)
+        solution.add(n.word);
+      results.add(solution);
+      pr("added solution:", solution);
+      return;
+    }
+
+   // pr(spaces(depth * 2), "find;", node.word);
+    checkState(!node.dead);
+    depth++;
+    path.add(node);
+    //    if (node == endNode) {
+    //      List<String> solution = new ArrayList<>(path.size());
+    //      for (var n : path)
+    //        solution.add(n.word);
+    //      results.add(solution);
+    //      pr("added solution:", solution);
+    //    }
+    if (withDeadEndFilter) {
+    checkState(node.links.size() != 0);
+    }
+    for (var sib : node.links) {
+      findResults(sib);
+    }
+    path.remove(path.size() - 1);
+    depth--;
   }
 
   private void constructGraph(List<String> words) {
@@ -122,19 +230,41 @@ public class P126WordLadder {
   private void filterEdges() {
     for (var n : mGraph.values()) {
       List<Node> filtered = new ArrayList<>();
-      for (var s : n.links) {
-        if (s.depth == n.depth + 1) {
-          filtered.add(s);
+      if (n != endNode) {
+        for (var s : n.links) {
+          if (s.depth == n.depth + 1) {
+            filtered.add(s);
+          }
         }
       }
       n.links = filtered;
     }
   }
 
+  private static final boolean withDeadEndFilter = true;
+  
+  private boolean auxFilterDeadEnd(Node node) {
+    if (node == endNode)
+      return true;
+    List<Node> filtered = new ArrayList<>();
+    for (var sib : node.links) {
+      if (!auxFilterDeadEnd(sib))
+        continue;
+      filtered.add(sib);
+    }
+    node.links = filtered;
+    if (filtered.isEmpty()) {
+      pr("node doesn't reach target:", node.word);
+      node.dead = true;
+    }
+    return !filtered.isEmpty();
+  }
+
   private static class Node {
     String word;
     List<Node> links = new ArrayList<>();
     int depth = -1;
+    boolean dead;
 
     Node(String w) {
       this.word = w;
@@ -151,5 +281,7 @@ public class P126WordLadder {
   private Map<String, Node> mGraph;
   private Node beginNode;
   private Node endNode;
+  private List<Node> path = new ArrayList<>();
+  private List<List<String>> results = new ArrayList<>();
 
 }
