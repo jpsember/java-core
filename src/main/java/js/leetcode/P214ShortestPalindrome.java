02e58d6c81c43a0bf9b57a8ffc03df78a2239e97
Jeff Sember <jpsember@gmail.com>
2023-11-28 12:11:04 -0800

    Issue #23

--- src/main/java/js/leetcode/P214ShortestPalindrome.java
+++ src/main/java/js/leetcode/P214ShortestPalindrome.java
@@ -4,7 +4,6 @@ package js.leetcode;
 import static js.base.Tools.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * <pre>
@@ -27,6 +26,9 @@ import java.util.List;
  *
  * I'm thinking that I won't construct explicit tables for the DP
  * implementation, rather lists of states.
+ * 
+ * Reworking the state storage to attempt to deal with very large strings
+ * without running out of memory.
  */
 public class P214ShortestPalindrome {
 
@@ -36,36 +38,38 @@ public class P214ShortestPalindrome {
   }
 
   private void run() {
+    for (int i = 1; i < 20; i++)
+      xRep("a", i);
+
     x("AH", "HAH");
 
     x("aacecaaa", "aaacecaaa");
-    // x("aacaaa", "aaacaaa");
 
     x("a", "a");
     x("abcd", "dcbabcd");
 
-    // This string of 50000 identical characters is causing memory problems.
-   if (false) {
-      int t = 50000;
-      var s = "a";
-      while (s.length() < t) {
-        s = s + s;
-      }
-      s = s.substring(0, t);
-      pr(s.length());
-      x(s, s);
-    }
+    xRep("a", 5000);
+  }
 
+  private void xRep(String pattern, int repCount) {
+    var sb = new StringBuilder(pattern.length() * repCount);
+    for (var i = 0; i < repCount; i++)
+      sb.append(pattern);
+    var s = sb.toString();
+    x(s, s);
   }
 
   private void x(String s, String expected) {
     var result = shortestPalindrome(s);
-    pr(s, "=>", result);
+    if (s.length() < 40)
+      pr(s, "=>", result);
     checkState(result.equals(expected), "expected:", expected);
   }
 
   public String shortestPalindrome(String s) {
-    pr(s);
+    final boolean tr = false ; //s.length() < 15;
+    if (tr)
+      pr(s);
 
     final var n = s.length();
 
@@ -73,7 +77,10 @@ public class P214ShortestPalindrome {
 
     // We'll store all states for a particular h in its own list, so we can just store the state as x.
 
-    var xUseful = (n - 1) / 2;
+    // Determine which values of x might be useful.  x must be <= this value.  Note, special care is
+    // taken to make sure this is correct for both even- and odd-length strings.
+    var xUseful = n / 2;
+
     var stackX = new ArrayList<Integer>(n);
     var stackXPlus1 = new ArrayList<Integer>(n);
     var stackXPlus2 = new ArrayList<Integer>(n);
@@ -87,19 +94,28 @@ public class P214ShortestPalindrome {
     int h = 0;
     while (!(stackX.isEmpty() && stackXPlus1.isEmpty())) {
 
-      pr(VERT_SP, quote(s), "h:", h, "stackX:", stackX, "X1:", stackXPlus1, "X2:", stackXPlus2);
+      if (tr)
+        pr(VERT_SP, quote(s), "S" + h + ":", stackX, "S" + (h + 1) + ":", stackXPlus1, "S" + (h + 2) + ":",
+            stackXPlus2);
+
+      //      if (!tr && h % 100 == 0)
+      //        pr("h:", h);
 
       for (var x : stackX) {
-        pr("state h:", h, "x:", x, "pal:", quote(s.substring(x, x + h)));
+        if (tr)
+          pr("...x:", x, "pal:", quote(s.substring(x, x + h)));
         if (x == 0) {
           if (h > longestPrefixLength) {
             longestPrefixLength = h;
-            pr("...new longest prefix:", longestPrefixLength);
+            if (tr)
+              pr("...new longest prefix:", longestPrefixLength);
           }
         } else {
           int j = x + h;
           if (j < n && s.charAt(x - 1) == s.charAt(j)) {
-            pr("....extending:", s.charAt(x - 1), s.substring(x, x + h), s.charAt(j));
+            if (tr)
+              pr("....extending to h" + (h + 2) + "[", x, "]:", s.charAt(x - 1), s.substring(x, x + h),
+                  s.charAt(j));
             stackXPlus2.add(x - 1);
           }
         }
@@ -110,7 +126,7 @@ public class P214ShortestPalindrome {
       stackXPlus1 = stackXPlus2;
       stackXPlus2 = tmp;
       stackXPlus2.clear();
-      pr("new stack X:", stackX, "X1:", stackXPlus1, "stackX2", stackXPlus2);
+      // pr("new stack X:", stackX, "X1:", stackXPlus1, "stackX2", stackXPlus2);
       h++;
     }
 
