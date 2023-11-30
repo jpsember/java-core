05c6e4ccf2adfef12ddf5c452223c8a4dd040fbe
Jeff Sember <jpsember@gmail.com>
2023-11-29 21:17:24 -0800

    Issue #23

--- src/main/java/js/leetcode/P212WordSearchII.java
+++ src/main/java/js/leetcode/P212WordSearchII.java
@@ -10,6 +10,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This is a more optimized (?) version, but my first version used what is apparently a 'Trie'.
+ * 
+ *
+ */
 public class P212WordSearchII {
 
   public static void main(String[] args) {
@@ -18,6 +23,13 @@ public class P212WordSearchII {
 
   private void run() {
     x(4, 4, "oaanetaeihkriflv", "oath pea eat rain", "eat oath");
+
+    x(3, 3, "abcaedafg", "abcdefg gfedcbaaa eaabcdgfa befa dgc ade", "abcdefg befa eaabcdgfa gfedcbaaa");
+
+    x(12, 12,
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "a aa aaa aaaa aaaaa aaaaaa aaaaaaa aaaaaaaa aaaaaaaaa aaaaaaaaaa",
+        "a aa aaa aaaa aaaaa aaaaaa aaaaaaa aaaaaaaa aaaaaaaaa aaaaaaaaaa");
   }
 
   private void x(int width, int height, String cells, String dicts, String expected) {
@@ -35,8 +47,33 @@ public class P212WordSearchII {
     checkState(exp.equals(result), "expected:", exp);
   }
 
+  //  @Override
+  //  public String toString() {
+  //    var sb = new StringBuilder();
+  //    for (int x = 0; x < bWidth; x++)
+  //      sb.append("---");
+  //    sb.append('\n');
+  //    for (int y = 0; y < bHeight; y++) {
+  //      for (int x = 0; x < bWidth; x++) {
+  //        var posCode = cellPos(x, y);
+  //        //var i = cellIndex(x, y);
+  //        int index = indexWithinVisitedList(posCode);
+  //        sb.append(index >= 0 ? " " + index : "  ");
+  //        sb.append(bChars[y][x]);
+  //      }
+  //      sb.append('\n');
+  //    }
+  //    for (int x = 0; x < bWidth; x++)
+  //      sb.append("---");
+  //    sb.append('\n');
+  //    return sb.toString();
+  //  }
+
   public List<String> findWords(char[][] board, String[] words) {
-    mBoard = new Board(board);
+
+    bChars = board;
+    bWidth = board[0].length;
+    bHeight = board.length;
     mResult.clear();
     mPrefixMap.clear();
 
@@ -45,117 +82,79 @@ public class P212WordSearchII {
         var pref = w.substring(0, i);
         Integer c = mPrefixMap.getOrDefault(pref, 0);
         int newVal = (i == w.length()) ? 2 : 1;
-        if (newVal > c)
+        if (newVal > c) {
           mPrefixMap.put(pref, newVal);
+        }
       }
     }
 
     mSb.setLength(0);
-    for (int y = 0; y < mBoard.height; y++)
-      for (int x = 0; x < mBoard.width; x++)
+    for (int y = bHeight - 1; y >= 0; y--)
+      for (int x = bWidth - 1; x >= 0; x--)
         auxFind(x, y);
 
     return new ArrayList<String>(mResult);
   }
 
   private void auxFind(int x, int y) {
-    var posCode = mBoard.tryVisit(x, y);
+    var vc = visitedCursor;
+    var posCode = tryVisit(x, y);
+
     if (posCode < 0)
       return;
 
     var len = mSb.length();
-    mSb.append(mBoard.c[y][x]);
+    mSb.append(bChars[y][x]);
     var wd = mSb.toString();
     var code = mPrefixMap.getOrDefault(wd, 0);
-    pr("code for", quote(wd), "is", code);
     if (code != 0) {
-      pr("prefix is in tree:", quote(wd));
       if (code == 2) {
-        pr("word is in tree:", quote(wd));
         mResult.add(wd);
       }
       auxFind(x - 1, y);
       auxFind(x + 1, y);
       auxFind(x, y - 1);
       auxFind(x, y + 1);
-    } else {
-      pr("prefix not in tree:", quote(wd));
     }
     mSb.setLength(len);
-    mBoard.visitedCursor--;
+    visitedCursor = vc;
   }
 
-  private static class Board {
-    int width;
-    int height;
-    char[][] c;
+  private int indexWithinVisitedList(int pos) {
+    for (int i = 0; i < visitedCursor; i++)
+      if (visitedList[i] == pos)
+        return i;
+    return -1;
+  }
 
-    Board(char[][] c) {
-      this.c = c;
-      width = c[0].length;
-      height = c.length;
-    }
+  private int cellPos(int x, int y) {
+    return (x | (y << 8));
+  }
 
-    @Override
-    public String toString() {
-      var sb = new StringBuilder();
-      for (int x = 0; x < width; x++)
-        sb.append("---");
-      sb.append('\n');
-      for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-          var posCode = cellPos(x, y);
-          //var i = cellIndex(x, y);
-          int index = indexWithinVisitedList(posCode);
-          sb.append(index >= 0 ? " " + index : "  ");
-          sb.append(c[y][x]);
-        }
-        sb.append('\n');
-      }
-      for (int x = 0; x < width; x++)
-        sb.append("---");
-      sb.append('\n');
-      return sb.toString();
-    }
-
-    private int indexWithinVisitedList(int pos) {
-      for (int i = 0; i < visitedCursor; i++)
-        if (visitedList[i] == pos)
-          return i;
+  /**
+   * Determine if a cell of the board can be moved to. It must be within the
+   * board, and cannot be marked as having been visited.
+   * 
+   * If so, it is marked as having been visited
+   */
+  public int tryVisit(int x, int y) {
+    if (x < 0 || x >= bWidth || y < 0 || y >= bHeight)
+      return -1;
+    var posCode = cellPos(x, y);
+    if (indexWithinVisitedList(posCode) >= 0) {
       return -1;
     }
-
-    private int cellPos(int x, int y) {
-      return x | (y * width);
-    }
-
-    /**
-     * Determine if a cell of the board can be moved to. It must be within the
-     * board, and cannot be marked as having been visited.
-     * 
-     * If so, it is marked as having been visited
-     */
-    public int tryVisit(int x, int y) {
-      pr("try visit:", x, y);
-      if (x < 0 || x >= width || y < 0 || y >= height)
-        return -1;
-      var posCode = cellPos(x, y);
-      if (indexWithinVisitedList(posCode) >= 0) {
-        pr("...already visited");
-        return -1;
-      }
-      visitedList[visitedCursor++] = posCode;
-      return posCode;
-    }
-
-    private int[] visitedList = new int[10];
-    private int visitedCursor;
-
+    visitedList[visitedCursor++] = posCode;
+    return posCode;
   }
 
-  private Board mBoard;
+  private int[] visitedList = new int[10 + 1];
+  private int visitedCursor;
   private Set<String> mResult = new HashSet<>();
   private StringBuilder mSb = new StringBuilder();
   private Map<String, Integer> mPrefixMap = new HashMap<>();
+  private int bWidth;
+  private int bHeight;
+  private char[][] bChars;
 
 }
