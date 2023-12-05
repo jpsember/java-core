e00f3c3b5e1f2f407f4665cbaf3c5352df92af6f
Jeff Sember <jpsember@gmail.com>
2023-12-05 12:29:43 -0800

    Issue #23

--- src/main/java/js/leetcode/P85MaximalRectangle.java
+++ src/main/java/js/leetcode/P85MaximalRectangle.java
@@ -108,40 +108,26 @@ public class P85MaximalRectangle {
     sScannedSubrectsSet.clear();
     int gridWidth = matrix[0].length;
     int gridHeight = matrix.length;
-
-    //    alert("turning on padding produces wrong answers");
-    final boolean pad = false;
-    if (pad) {
-      bWidth = gridWidth + 2;
-      bHeight = gridHeight + 2;
-    } else {
-      bWidth = gridWidth;
-      bHeight = gridHeight;
-    }
+    bWidth = gridWidth;
+    bHeight = gridHeight;
     byte[] cells = new byte[bWidth * bHeight];
     sCells = cells;
     int ci = 0;
-    if (pad)
-      ci = bWidth + 1;
     for (var row : matrix) {
       for (var c : row) {
         if (c == '1')
           cells[ci] = 1;
         ci++;
       }
-      if (pad)
-        ci += 2;
     }
     return subscan(0, bWidth, 0, bHeight, 0);
   }
 
   private int subscan(int x0, int x1, int y0, int y1, int maxArea) {
-    //showBoard(x0, x1, y0, y1, "subscan", x0, y0, "area:", maxArea);
-
     var area = (x1 - x0) * (y1 - y0);
-    if (area <= maxArea) {
+    if (area <= maxArea)
       return maxArea;
-    }
+    //showBoard(x0, x1, y0, y1, "subscan", x0, y0, "area:", maxArea);
 
     // If we've already scanned (or are currently scanning) this particular subrect,
     // exit
@@ -153,27 +139,23 @@ public class P85MaximalRectangle {
     var ymid = (y1 + y0) >> 1;
 
     var cells = sCells;
-    var bRow = bWidth;
-    int cellInd = bRow * y0;
-
     int bestx = 0;
     int besty = 0;
-
     var minDist = 100000;
-
     boolean empty = true;
     boolean full = true;
-
-    for (int y = y0; y < y1; y++, cellInd += bRow) {
+    var rowOffset = bWidth - (x1 - x0);
+    int cellInd = bWidth * y0 + x0;
+    for (int y = y0; y < y1; y++, cellInd += rowOffset) {
+      var yDistComponent = (y - ymid) * (y - ymid);
       for (int x = x0; x < x1; x++) {
-        int c = cellInd + x;
-        var cCurr = cells[c];
+        var cCurr = cells[cellInd++];
         if (cCurr == 1) {
           empty = false;
           continue;
         }
         full = false;
-        var dist = (x - xmid) * (x - xmid) + (y - ymid) * (y - ymid);
+        var dist = (x - xmid) * (x - xmid) + yDistComponent;
         if (dist < minDist) {
           minDist = dist;
           bestx = x;
@@ -185,13 +167,10 @@ public class P85MaximalRectangle {
       return Math.max(area, maxArea);
     if (empty)
       return maxArea;
-
     maxArea = subscan(x0, bestx, y0, y1, maxArea);
     maxArea = subscan(bestx + 1, x1, y0, y1, maxArea);
-
     maxArea = subscan(x0, x1, besty + 1, y1, maxArea);
     maxArea = subscan(x0, x1, y0, besty, maxArea);
-
     return maxArea;
   }
 
