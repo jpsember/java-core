acd9a1e7655eb9af82725c9c5bc627b8d77d85f0
Jeff Sember <jpsember@gmail.com>
2023-12-08 10:49:34 -0800

    Issue #23

--- src/main/java/js/leetcode/P154FindMinimumInSortedArrayII.java
+++ src/main/java/js/leetcode/P154FindMinimumInSortedArrayII.java
@@ -2,6 +2,7 @@
 package js.leetcode;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Random;
 
@@ -36,14 +37,27 @@ public class P154FindMinimumInSortedArrayII {
   }
 
   private void run() {
-    x("7777777777777777777777777777777777777777777776777777777");
-    y(7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 7, 7, 7, 7,
-        7, 7);
-    x(1965);
+      y("","0",100,"10");
+      y("01","0",100,"");
+      
+//    y("000000000000000000000000000000001","0",100,"0");
+//    y("01","0",100,"0");
+//   
+//      x(1965,500);
   }
 
   private void x(int seed) {
     x(seed, 16);
+  }
+
+  private void y(String prefix, String mid, int length, String suffix) {
+    StringBuilder sb = new StringBuilder(prefix);
+    int j = sb.length();
+    while (sb.length() < j + length)
+      sb.append(mid);
+    sb.setLength(j + length);
+    sb.append(suffix);
+    x(sb.toString());
   }
 
   private void x(String vals) {
@@ -80,6 +94,7 @@ public class P154FindMinimumInSortedArrayII {
     int min = nums[0];
     for (var v : nums)
       min = Math.min(min, v);
+    mRead.clear();
     int result = findMin(nums);
     pr(nums, "result:", result);
     checkState(result == min, "expected:", min);
@@ -90,66 +105,77 @@ public class P154FindMinimumInSortedArrayII {
   }
 
   public int findMin(int[] nums) {
+    mNums = nums;
+    inf = 0;
+    mReadCount = 0;
     minValueSoFar = null;
-    return auxFindMin(nums, 0, 0, nums.length);
+    auxFindMin(0, nums.length);
+
+    pr("read count:", mReadCount, ((100 * mReadCount) / mNums.length) + "%");
+    return minValueSoFar;
   }
 
-  private int auxFindMin(int[] nums, int modIndex, int a, int b) {
-    checkState(modIndex == 0);
+  private void auxFindMin(int a, int b) {
+    checkState(inf++ < 3000);
     int diff = b - a;
-    pr("auxFindMin, modIndex:", modIndex, "a:", a, "b:", b, "diff:", diff);
+    if (diff <= 0)
+      return;
+    pr("auxFindMin a:", a, "b:", b, "diff:", diff, "nums.length:", mNums.length);
     if (diff < 3) {
-      for (int i = a; i < b; i++) {
-        get(nums, modIndex, i, "(range of < 3)");
+      for (int i = 0; i < diff; i++) {
+        get(i + a, "(range of < 3)");
       }
-      return minValueSoFar;
+      return;
     }
     int sep = diff / 3;
     int u = a;
     int v = a + sep;
     int w = b - sep;
-    pr("sep:", sep, "u:", u, "v:", v, "w:", w);
+    //pr("...sep:", sep, "u:", u, "v:", v, "w:", w);
     checkState(u != v && v != w && w != u);
-    pr("...reading u:", u);
-    int nu = get(nums, modIndex, u, "u");
-    pr("...reading v:", v);
-    int nv = get(nums, modIndex, v, "v");
-    pr("...reading w:", w);
-    int nw = get(nums, modIndex, w, "w");
-    pr("...u:", u, nu, "v:", v, nv, "w:", w, nw);
+    int nu = get(u, "u");
+    int nv = get(v, "v");
+    int nw = get(w, "w");
+    //pr("...u:", u, nu, "v:", v, nv, "w:", w, nw);
     if (nu > nv) {
       pr("...nu>nv, assuming in u...v region");
-      return auxFindMin(nums, modIndex, u + 1, v - 1);
+      auxFindMin(u + 1, v - 1);
     } else if (nv > nw) {
       pr("...nv>nw, assuming in v...w region");
-      return auxFindMin(nums, modIndex, v + 1, w - 1);
+      auxFindMin(v + 1, w - 1);
     } else if (nw > nu) {
       pr("...nw>nu, assuming in < u or > w region");
-      //var modAdj = w + 1;
-      return auxFindMin(nums, modIndex, w + 1, u - 1 + nums.length);
+      auxFindMin(w + 1, b);
     } else {
       pr("...nu,nv,nw all same");
       int oldMin = minValueSoFar;
       pr("...scanning u+1..v-1");
-      auxFindMin(nums, modIndex, u + 1, v - 1);
+      auxFindMin(u + 1, v - 1);
       if (oldMin == minValueSoFar) {
-        pr("......haven't found new min, scanning v+1...w-1");
-        auxFindMin(nums, modIndex, v + 1, w - 1);
+        pr("......haven't found new min, scanning v+1...w-1", v + 1, w - 1);
+        auxFindMin(v + 1, w - 1);
       }
       if (oldMin == minValueSoFar) {
-       // var modAdj = w + 1;
-        pr("......haven't found new min, scanning w+1...u-1");
-        auxFindMin(nums, modIndex , w+1,  u - 1 +nums.length);
+        pr("......haven't found new min, scanning w+1...b-1", w + 1, b - 1);
+        //checkState(w + 1 < u - 1 + mNums.length);
+        auxFindMin(w + 1, b - 1);
       }
-      return minValueSoFar;
     }
   }
 
-  private int get(int[] nums, int modIndex, int index, String prompt) {
-    int effInd = index - modIndex;
-    if (effInd < 0)
-      effInd += nums.length;
-    checkState(effInd >= 0 && effInd < nums.length);
+  int inf;
+  private int mReadCount;
+
+  private int get(int index, String prompt) {
+    var nums = mNums;
+    int effInd = index % nums.length;
+    if (mRead.get(effInd)) {
+      pr("*** Already read index:", index, prompt);
+      halt();
+    } else
+      mRead.set(effInd);
+    mReadCount++;
+
     var value = nums[effInd];
     boolean upd = false;
     if (minValueSoFar == null || minValueSoFar > value) {
@@ -161,5 +187,6 @@ public class P154FindMinimumInSortedArrayII {
   }
 
   private Integer minValueSoFar;
-
+  private int[] mNums;
+  private BitSet mRead = new BitSet();
 }
