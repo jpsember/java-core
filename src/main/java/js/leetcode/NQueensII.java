b8de04bdbf0e9bbc4801c9e8227f7b5654692b25
Jeff Sember <jpsember@gmail.com>
2023-12-16 14:18:19 -0800

    Issue #23

--- src/main/java/js/leetcode/NQueensII.java
+++ src/main/java/js/leetcode/NQueensII.java
@@ -19,7 +19,7 @@ public class NQueensII extends LeetCode {
   private void x(int n, int exp) {
     var c = totalNQueens(n);
     pr("n:", n, "c:", c);
-    verify(c, n);
+    verify(c, exp);
   }
 
   private static final int N = 10;
@@ -33,8 +33,9 @@ public class NQueensII extends LeetCode {
     return count;
   }
 
-  public int totalNQueens(int n) {
-    var squareFlags = new long[N][N];
+  private static final long[][] squareFlags = new long[N][N];
+
+  static {
     for (int y = 0; y < N; y++) {
       var row = squareFlags[y];
       for (int x = 0; x < N; x++) {
@@ -46,36 +47,37 @@ public class NQueensII extends LeetCode {
             | (1L << (OFF_ROW + y)) //
             | (1L << (OFF_DIAG1 + x + y)) //
             | (1L << (OFF_DIAG2 + x - y + N - 1));
-        //pr("y:", y, "x:", x, "f:", bitStr(row[x] | (1L << 62)));
       }
     }
+  }
 
-    Map<Long, Integer> countMapA = new HashMap<>();
+  public int totalNQueens(int n) {
+    Map<Long, Integer> prevSizeMap = new HashMap<>();
+    Map<Long, Integer> nextSizeMap = new HashMap<>();
+
     // There is a single solution for n=0 queens in the n=0 map
-    countMapA.put(0L, 1);
-    // Auxilliary map 
-    Map<Long, Integer> countMapB = new HashMap<>();
+    prevSizeMap.put(0L, 1);
 
-    int sum = 0;
+    int currentVariantTotal = 0;
     for (int m = 1; m <= n; m++) {
-      sum = 0;
-      countMapB.clear();
+      currentVariantTotal = 0;
+      nextSizeMap.clear();
       db(VERT_SP, "size", m);
 
       // Look through all the entries for map from m-1
-      for (var ent : countMapA.entrySet()) {
+      pr("prev map size:", prevSizeMap.size());
+      for (var ent : prevSizeMap.entrySet()) {
         long used = ent.getKey();
         int queenCount = countQueens(used);
         int variants = ent.getValue();
-        pr("map entry; # queens:", queenCount, "variants:", variants);
-        // pr("rowFlags:", bitStr(used), "count:", variants, "queens:", queenCount);
+        db("map entry; # queens:", queenCount, "variants:", variants);
 
-        // Try to place one and two additional queens to store size-1 and size-0 solutions
+        // Try to place one and two additional queens to get solutions for m-1 and m queens
 
         // If this is already an m-1 solution, store it
         if (queenCount == m - 1) {
-          pr("...already an m-1 solution, storing");
-          countMapB.put(used, variants);
+          db("...already an m-1 solution, storing");
+          nextSizeMap.put(used, variants);
         }
         int scanCount = m * 2 - 1;
         for (int i = 0; i < scanCount; i++) {
@@ -89,14 +91,19 @@ public class NQueensII extends LeetCode {
           }
 
           var sf = squareFlags[y][x];
-          pr("x:", x, "y:", y, "f:", bitStr(sf));
           if ((sf & used) == 0) {
-            pr("...found slot for queen #" + (queenCount + 1), "x:", x, "y:", y);
-            countMapB.put(used | sf, variants);
-            queenCount++;
+            var usedNew = used | sf;
+            var newQueenCount = queenCount + 1;
+
+            nextSizeMap.put(used | sf, variants);
+            pr("found spot for queen #", newQueenCount, "at:", x, y);
+            if (newQueenCount == m) {
+              currentVariantTotal += variants;
+              pr("...variants now", currentVariantTotal);
+            }
             // See if we can place an additional queen for an m solution
-            if (queenCount + 1 == m) {
-              pr("...scanning for a queen #" + queenCount, "solution");
+            if (newQueenCount + 1 == m) {
+
               for (int i2 = i + 1; i2 < scanCount; i2++) {
                 if (i2 < m) {
                   y = m - 1;
@@ -105,47 +112,23 @@ public class NQueensII extends LeetCode {
                   y = i2 - m;
                   x = m - 1;
                 }
-                pr("scanCount:", scanCount, "i2:", i2, "x:", x, "y:", y);
                 sf = squareFlags[y][x];
-                pr("x:", x, "y:", y, "f:", bitStr(sf));
-                if ((sf & used) == 0) {
-                  countMapB.put(used | sf, variants);
-                  sum += variants;
-                  pr("...found slot for queen #" + (queenCount + 1), "x:", x, "y:", y);
-                  pr("...found size m solution, # variants:", variants);
+                if ((sf & usedNew) == 0) {
+                  nextSizeMap.put(usedNew | sf, variants);
+                  currentVariantTotal += variants;
+                  db("...found slot for queen #" + (newQueenCount + 1), "x:", x, "y:", y, "variants now:",
+                      variants);
                 }
               }
             }
           }
         }
-        //        {
-        //          var x = size - 1;
-        //          for (int y = 1; y < size; y++) {
-        //            var sf = squareFlags[y][x];
-        //            pr("x:", x, "y:", y, "f:", bitStr(sf));
-        //            if ((sf & used) == 0) {
-        //              newBoardKeys.add(sf | used);
-        //              newBoardValues.add(count);
-        //              sum += count;
-        //            }
-        //          }
-        //        }
       }
-      var tmp = countMapA;
-      countMapA = countMapB;
-      countMapB = tmp;
-      //
-      //      for (int i = 0; i < newBoardKeys.size(); i++)
-      //        countMap.put(newBoardKeys.get(i), newBoardValues.get(i));
-      db("total for n=", m, "is", sum);
-
+      var tmp = prevSizeMap;
+      prevSizeMap = nextSizeMap;
+      nextSizeMap = tmp;
+      db("total for n=", m, "is", currentVariantTotal);
     }
-    return sum;
+    return currentVariantTotal;
   }
-  //
-  //  private static class Solution {
-  //    int boardSize;
-  //    int numQueens;
-  //  }
-
 }
