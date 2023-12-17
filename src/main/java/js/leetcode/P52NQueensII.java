68012315bb3bd9049b16b46d338137235e7ff599
Jeff Sember <jpsember@gmail.com>
2023-12-16 18:58:34 -0800

    Issue #23

--- src/main/java/js/leetcode/P52NQueensII.java
+++ src/main/java/js/leetcode/P52NQueensII.java
@@ -34,16 +34,19 @@ public class P52NQueensII extends LeetCode {
 
   public int totalNQueens(int n) {
     // Prepare grid indicating how a queen placed on a square affects the row usage
-    long[][] squareFlags = new long[n][n];
-    for (int y = 0; y < n; y++) {
-      var row = squareFlags[y];
-      for (int x = 0; x < n; x++) {
-        final int OFF_COL = 0;
-        final int OFF_DIAG1 = OFF_COL + n;
-        final int OFF_DIAG2 = OFF_DIAG1 + n * 2 - 1;
-        row[x] = (1L << (OFF_COL + x)) //
-            | (1L << (OFF_DIAG1 + x + y)) //
-            | (1L << (OFF_DIAG2 + x - y + n - 1));
+    var squareFlags = new long[n * n];
+    {
+      int i = 0;
+      for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++, i++) {
+
+          final int OFF_COL = 0;
+          final int OFF_DIAG1 = OFF_COL + n;
+          final int OFF_DIAG2 = OFF_DIAG1 + n * 2 - 1;
+          squareFlags[i] = (1L << (OFF_COL + x)) //
+              | (1L << (OFF_DIAG1 + x + y)) //
+              | (1L << (OFF_DIAG2 + x - y + n - 1));
+        }
       }
     }
     Map<Long, Integer> prevHeightMap = new HashMap<>();
@@ -52,20 +55,19 @@ public class P52NQueensII extends LeetCode {
     // There is a single solution for n=0 queens in the height=zero map
     prevHeightMap.put(0L, 1);
     int solutionCount = 0;
-    for (int height = 1; height <= n; height++) {
+    int sqOffset = 0;
+    for (int height = 1; height <= n; height++, sqOffset += n) {
       solutionCount = 0;
       nextHeightMap.clear();
       for (var ent : prevHeightMap.entrySet()) {
         long rowUsageFlags = ent.getKey();
-        int queenCount = Integer.bitCount(((int) rowUsageFlags) & ((1 << n) - 1));
         var solutions = ent.getValue();
         for (int x = 0; x < n; x++) {
-          var candidateMoveFlags = squareFlags[height - 1][x];
+          var candidateMoveFlags = squareFlags[sqOffset + x];
           if ((candidateMoveFlags & rowUsageFlags) == 0) {
             var newUsageFlags = rowUsageFlags | candidateMoveFlags;
             nextHeightMap.put(newUsageFlags, nextHeightMap.getOrDefault(newUsageFlags, 0) + solutions);
-            if (queenCount + 1 == height)
-              solutionCount += solutions;
+            solutionCount += solutions;
           }
         }
       }
