f7031ce5a05c2a53a34000c9769e4bd1bc9980c4
Jeff Sember <jpsember@gmail.com>
2023-12-19 14:21:38 -0800

    Issue #23

--- src/main/java/js/leetcode/SubstringwithConcatenationofAllWords.java
+++ src/main/java/js/leetcode/SubstringwithConcatenationofAllWords.java
@@ -3,6 +3,7 @@ package js.leetcode;
 import static js.base.Tools.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +11,10 @@ import java.util.Map;
 import js.json.JSList;
 
 /**
- * First, slow version, failed since apparently the same word can appear multiple times in the words array.
+ * First, slow version, failed since apparently the same word can appear
+ * multiple times in the words array.
+ * 
+ * Fixed it, and the slow version beats 72.46% runtime, 10% memory
  */
 public class SubstringwithConcatenationofAllWords extends LeetCode {
 
@@ -22,6 +26,8 @@ public class SubstringwithConcatenationofAllWords extends LeetCode {
   public void run() {
 
     x("barfoothefoobarman", "[\"foo\",\"bar\"]");
+
+    x("wordgoodgoodgoodbestword", "[\"word\",\"good\",\"best\",\"good\"]");
 
   }
 
@@ -40,26 +46,50 @@ public class SubstringwithConcatenationofAllWords extends LeetCode {
     var wordLength = words[0].length();
     int concatWordLength = words.length * wordLength;
 
-    Map<String, Integer> wordIndexMap = new HashMap<>(words.length);
-    for (int i = 0; i < words.length; i++)
-      wordIndexMap.put(words[i], i);
+    Map<String, WordInfo> wordIndexMap = new HashMap<>(words.length);
+    int unique = 0;
+    for (int i = 0; i < words.length; i++) {
+      var info = wordIndexMap.get(words[i]);
+      if (info == null) {
+        info = new WordInfo();
+        wordIndexMap.put(words[i], info);
+        info.uniqueIndex = unique++;
+      }
+      info.multiplicity++;
+    }
+
+    int uniqueWordsCount = wordIndexMap.size();
+
+    int[] foundCount = new int[uniqueWordsCount];
+    int[] targetCount = new int[uniqueWordsCount];
+
+    for (var ent : wordIndexMap.entrySet()) {
+      var info = ent.getValue();
+      targetCount[info.uniqueIndex] = info.multiplicity;
+    }
 
     int cursorStop = s.length() - concatWordLength + 1;
     outer: for (int cursor = 0; cursor < cursorStop; cursor++) {
-      var ss = s.substring(cursor, cursor + concatWordLength);
-      int wordsFoundBitmap = 0;
-      for (int i = 0; i < words.length; i++) {
-        var word = ss.substring(i * wordLength, i * wordLength + wordLength);
-        var wordNumber = wordIndexMap.getOrDefault(word, -1);
-        if (wordNumber < 0)
+      Arrays.fill(foundCount, 0);
+      var ws = cursor;
+      for (int i = 0; i < words.length; i++, ws += wordLength) {
+        var word = s.substring(ws, ws + wordLength);
+        var info = wordIndexMap.get(word);
+        if (info == null)
           continue outer;
-        var bitFlag = 1 << wordNumber;
-        if ((wordsFoundBitmap & bitFlag) != 0)
+        int j = info.uniqueIndex;
+        if (foundCount[j] == targetCount[j])
           continue outer;
-        wordsFoundBitmap |= bitFlag;
+        foundCount[j]++;
       }
       result.add(cursor);
     }
     return result;
   }
+
+  private class WordInfo {
+    int uniqueIndex;
+    int multiplicity;
+  }
+
 }
