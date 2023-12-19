e1e46411a4a24e08950a87a09ae67c335570ebab
Jeff Sember <jpsember@gmail.com>
2023-12-19 12:57:49 -0800

    Issue #23

--- src/main/java/js/leetcode/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/js/leetcode/SerializeAndDeserializeBinaryTree.java
@@ -14,15 +14,47 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
 
   public void run() {
 
-    var tree = t(3, t(5, null, t(2)), t(7, t(-1000, t(4), t(-6))));
-    var c = new CodecLC();
-    var s = c.serialize(tree);
-    pr(s);
-    TreeNode tout = c.deserialize(s);
-    pr("tree in :", tree);
-    pr("tree out:", tout);
+    x("[3,5,7,null,2,-1000,null,null,null,4,-6]");
 
-    verify(tree.toString(), tout.toString());
+    if (false) {
+      var tree = t(3, t(5, null, t(2)), t(7, t(-1000, t(4), t(-6))));
+
+      if (false) {
+        var cc = new CodecLC();
+        var s = cc.serialize(tree);
+        pr("serialized:", s);
+        var result = cc.deserialize(s);
+        pr("deserialized:", result);
+        var s2 = cc.serialize(result);
+        verify(s, s2);
+      }
+
+      var c = new Codec();
+      var s = c.serialize(tree);
+      pr(s);
+      TreeNode tout = c.deserialize(s);
+      pr("tree in :", tree);
+      pr("tree out:", tout);
+
+      verify(tree.toString(), tout.toString());
+    }
+  }
+
+  private void x(String s) {
+
+    if (s.startsWith("[")) {
+      var c2 = new CodecLC();
+      var tree = c2.deserialize(s);
+      s = new Codec().serialize(tree);
+      pr("Serialized to our form:", s);
+    }
+    Codec c = new Codec();
+
+    var tree = c.deserialize(s);
+    var s2 = c.serialize(tree);
+    pr("Serialized:", INDENT, s);
+    pr("Deserialized:", INDENT, tree);
+    verify(s2, s);
   }
 
   private static TreeNode t(int val) {
@@ -35,10 +67,6 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
 
   private static TreeNode t(int val, TreeNode left, TreeNode right) {
     return new TreeNode(val, left, right);
-  }
-
-  private void x(String treeStr) {
-
   }
 
   private static class TreeNode {
@@ -55,19 +83,37 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
     @Override
     public String toString() {
       var sb = new StringBuilder();
-      auxStr(sb, this);
+      auxStr(sb, this, 0);
       return sb.toString();
     }
 
-    private void auxStr(StringBuilder sb, TreeNode node) {
-      if (node == null)
-        return;
-      sb.append(node.val);
-      sb.append('[');
-      auxStr(sb, node.left);
-      sb.append('|');
-      auxStr(sb, node.right);
-      sb.append("]");
+    private void auxStr(StringBuilder sb, TreeNode node, int indent) {
+      boolean compact = true;
+      String sp;
+      if (compact)
+        sp = " ";
+      else
+        sp = spaces(indent);
+      if (node == null) {
+        sb.append(sp);
+        sb.append("null");
+      } else {
+        if (node.left == null && node.right == null) {
+          sb.append(sp);
+          sb.append(node.val);
+        } else {
+          sb.append(sp);
+          sb.append('(');
+          sb.append(node.val);
+          if (!compact)
+            sb.append('\n');
+          auxStr(sb, node.left, indent + 2);
+          if (!compact)
+            sb.append('\n');
+          auxStr(sb, node.right, indent + 2);
+          sb.append(')');
+        }
+      }
     }
   }
 
@@ -108,38 +154,29 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
         }
       }
       trimSuffix(sb, ".");
-      //      if (true) {
-      //        int newLen = sb.length();
-      //        int i = newLen - 1;
-      //        while (i >= 0 && sb.charAt(i) == '.') {
-      //          newLen = i;
-      //          i--;
-      //        }
-      //        sb.setLength(newLen);
-      //      }
       return sb.toString();
     }
 
-    private int cursor;
-    private String text;
+    protected int cursor;
+    protected String text;
 
-    private boolean hasMore() {
+    protected final boolean hasMore() {
       return cursor < text.length();
     }
 
-    private char peek() {
+    protected final char peek() {
       if (hasMore())
         return text.charAt(cursor);
       return 0;
     }
 
-    private char read() {
+    protected final char read() {
       char c = peek();
       cursor++;
       return c;
     }
 
-    private boolean readIf(char c2) {
+    protected final boolean readIf(char c2) {
       if (peek() == c2) {
         cursor++;
         return true;
@@ -147,20 +184,27 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
       return false;
     }
 
-    private char read(char c2) {
-      if (peek() != c2) {
-        throw new IllegalArgumentException();
-      }
+    protected final char read(char c2) {
+      checkState(peek() == c2);
       return read();
+    }
+
+    protected final boolean readIf(String s) {
+      var cnew = cursor + s.length();
+      if (cnew <= text.length() && text.substring(cursor, cnew).equals(s)) {
+        cursor = cnew;
+        return true;
+      }
+      return false;
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
       db("deserialize:", quote(data));
-
-      List<Integer> values = new ArrayList<>();
       cursor = 0;
       text = data;
+
+      List<Integer> values = new ArrayList<>();
       while (hasMore()) {
         db("...peek:", Character.toString(peek()));
         Integer value = null;
@@ -189,18 +233,35 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
         db("...adding value:", value);
         values.add(value);
       }
-
-      TreeNode result = parse(values, 0);
-      return result;
+      return constructTreeFromValues(values);
     }
 
-    private TreeNode parse(List<Integer> values, int cursor) {
-      if (cursor >= values.size())
-        return null;
-      Integer val = values.get(cursor++);
-      if (val == null)
-        return null;
-      return new TreeNode(val, parse(values, cursor + 1), parse(values, cursor + 1));
+    protected TreeNode constructTreeFromValues(List<Integer> values) {
+      var treeQueue = new ArrayList<TreeNode>();
+      int treeCursor = 0;
+      int cursor = 0;
+      var result = new TreeNode(values.get(cursor++), null, null);
+      treeQueue.add(result);
+      while (treeCursor < treeQueue.size()) {
+        var tree = treeQueue.get(treeCursor++);
+        Integer val = null;
+        if (cursor < values.size())
+          val = values.get(cursor++);
+        if (val != null) {
+          var child = new TreeNode(val, null, null);
+          treeQueue.add(child);
+          tree.left = child;
+        }
+        val = null;
+        if (cursor < values.size())
+          val = values.get(cursor++);
+        if (val != null) {
+          var child = new TreeNode(val, null, null);
+          treeQueue.add(child);
+          tree.right = child;
+        }
+      }
+      return result;
     }
   }
 
@@ -215,6 +276,9 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
     }
   }
 
+  /**
+   * LeetCode tree serializer
+   */
   public class CodecLC extends Codec {
 
     public String serialize(TreeNode root) {
@@ -230,12 +294,11 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
         while (cursor < frontier.size()) {
           db("...cursor:", cursor, "string:", quote(sb.toString()));
           var n = frontier.get(cursor++);
-          db("...cursor:", cursor, "tree:", n);
-          if (sb.length() > 1) // && sb.charAt(sb.length() - 1) != '[')
+          if (sb.length() > 1)
             sb.append(',');
-          if (n == null) {
+          if (n == null)
             sb.append("null");
-          } else {
+          else {
             sb.append(n.val);
             frontier.add(n.left);
             frontier.add(n.right);
@@ -247,96 +310,43 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
       return sb.toString();
     }
 
-    private int cursor;
-    private String text;
-
-    private boolean hasMore() {
-      return cursor < text.length();
-    }
-
-    private char peek() {
-      if (hasMore())
-        return text.charAt(cursor);
-      return 0;
-    }
-
-    private char read() {
-      char c = peek();
-      cursor++;
-      return c;
-    }
-
-    private boolean readIf(char c2) {
-      if (peek() == c2) {
-        cursor++;
-        return true;
-      }
-      return false;
-    }
-
-    private boolean readIf(String s) {
-      var cnew = cursor + s.length();
-      if (cnew <= text.length() && text.substring(cursor, cnew).equals(s)) {
-        cursor = cnew;
-        return true;
-      }
-      return false;
-    }
-
-    private char read(char c2) {
-      if (peek() != c2) {
-        throw new IllegalArgumentException();
-      }
-      return read();
-    }
-
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
       db("deserialize:", quote(data));
+      cursor = 0;
+      text = data;
       TreeNode result = null;
       if (!readIf("null")) {
         read('[');
-
         List<Integer> values = new ArrayList<>();
-        cursor = 0;
-        text = data;
-        while (hasMore()) {
-          db("...peek:", Character.toString(peek()));
+        while (!readIf(']')) {
+          db("...remaining text:", quote(text.substring(cursor)));
           Integer value = null;
           if (!readIf("null")) {
-
+            int number = 0;
             boolean neg = readIf('-');
-            int i = 0;
             int oldCursor = cursor;
             while (true) {
               int digit = peek() - '0';
               if (digit < 0 || digit > 9)
                 break;
               read();
-              i = i * 10 + digit;
+              number = number * 10 + digit;
             }
-            if (cursor == oldCursor)
-              throw new IllegalArgumentException();
-            value = i;
+            checkState(cursor > oldCursor);
+            if (neg)
+              number = -number;
+            value = number;
           }
+          readIf(',');
+
           db("...adding value:", value);
           values.add(value);
         }
-
-        result = parse(values, 0);
-        read(']');
+        result = constructTreeFromValues(values);
       }
       checkState(cursor == text.length(), "extra characters");
       return result;
-    }
-
-    private TreeNode parse(List<Integer> values, int cursor) {
-      if (cursor >= values.size())
-        return null;
-      Integer val = values.get(cursor++);
-      if (val == null)
-        return null;
-      return new TreeNode(val, parse(values, cursor + 1), parse(values, cursor + 1));
     }
   }
 
