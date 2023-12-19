6154cd80f86e596ed806654c8377b9150b78b722
Jeff Sember <jpsember@gmail.com>
2023-12-18 18:09:43 -0800

    Issue #23

--- src/main/java/js/leetcode/FindMedianFromDataStream.java
+++ src/main/java/js/leetcode/FindMedianFromDataStream.java
@@ -23,6 +23,53 @@ import js.json.JSObject;
  * special case is made for the cursors pointing to values that are adjacent but
  * are split between two buckets.
  *
+ *
+ * To calculate a median, we can discard equal-sized "chunks" of numbers that
+ * lie on opposite sides of the median, and that these chunks do not need to be
+ * sorted.
+ * 
+ * The underlying data structure is an array of buckets, where each bucket
+ * contains an array of unsorted numbers. The array of buckets is sorted so that
+ * B(i)'s min and max numbers are strictly less than that of B(i+1)'s.
+ * 
+ * With each AddNum(x) call, we determine which of the existing buckets should
+ * receive x. We choose the bucket whose min and max range already contain x, or
+ * if no such bucket exists, the bucket whose distance from this range is
+ * minimized.
+ * 
+ * For FindMedian(), we set up a pair of cursors to walk from the first and last
+ * buckets towards the center. Each cursor has a pointer to a bucket and a count
+ * of the numbers processed by the procedure. At each step, we add the smaller
+ * bucket's population to each count, and move the bucket pointers inward when
+ * all the bucket's numbers are consumed. We do this until either the two
+ * cursors point at the same bucket. At this point, if the bucket is small
+ * enough, we sort that bucket's contents, and use the cursor counts to
+ * determine the median. A special case exists when one cursor is pointing to
+ * the max value in one bucket, and the next cursor is pointing to the minimum
+ * value in the adjacent bucket. The median is then the average of one bucket's
+ * min() and the other bucket's max().
+ * 
+ * If the above procedure reaches a bucket whose size is large enough, then
+ * instead of sorting the bucket, we split the bucket by partitioning its
+ * numbers based on the average of the bucket's min and max values (if min =
+ * max, then all the numbers in the bucket are identical, and no splitting or
+ * sorting is required). We replace the original bucket with the two smaller
+ * buckets, and repeat the FindMedian() procedure.
+ * 
+ * The expensive sorting step in FindMedian() can be optimized by keeping track
+ * of how many of a bucket's numbers are already sorted. When adding new numbers
+ * to an already-sorted bucket, the numbers are appended to the end of the
+ * array. When sorting is next required, we sort only the range of new numbers
+ * (at the end of the array), then perform a linear, in-space merge of the two
+ * subarrays.
+ * 
+ * 
+ * $$O(n log n)$$ in the worst case, due to the bucket sorting. (The linear scan
+ * of buckets in AddNum() might imply a running time of $$O(n^2)$$, but this
+ * could only happen if the bucket count was a significant fraction of $$n$$,
+ * which is not the case: buckets are only created when a bucket gets large,
+ * which implies the bucket count is bounded by $$O(log n)$$).
+ * 
  */
 public class FindMedianFromDataStream extends LeetCode {
 
