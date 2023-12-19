1a4f07c2862e3920b01b204dda0227e78b56f233
Jeff Sember <jpsember@gmail.com>
2023-12-19 11:09:17 -0800

    Issue #23

--- src/main/java/js/leetcode/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/js/leetcode/SerializeAndDeserializeBinaryTree.java
@@ -5,67 +5,6 @@ import static js.base.Tools.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * The running time is in the bottom 20%.
- * 
- * Now using an approach where we add values to a bucket, unsorted, until a
- * median request happens. At that point, if there are multiple buckets, we walk
- * from the left and right sides of the bucket list, "chopping" off buckets
- * since a 'low' bucket will be balanced by a 'high' bucket of the same size.
- * When both cursors reach the same bucket, then we either split that bucket (if
- * its size is large) or we sort the bucket and extract the median (with special
- * care for the cursor positions with which we arrived at that bucket). A
- * special case is made for the cursors pointing to values that are adjacent but
- * are split between two buckets.
- *
- *
- * To calculate a median, we can discard equal-sized "chunks" of numbers that
- * lie on opposite sides of the median, and that these chunks do not need to be
- * sorted.
- * 
- * The underlying data structure is an array of buckets, where each bucket
- * contains an array of unsorted numbers. The array of buckets is sorted so that
- * B(i)'s min and max numbers are strictly less than that of B(i+1)'s.
- * 
- * With each AddNum(x) call, we determine which of the existing buckets should
- * receive x. We choose the bucket whose min and max range already contain x, or
- * if no such bucket exists, the bucket whose distance from this range is
- * minimized.
- * 
- * For FindMedian(), we set up a pair of cursors to walk from the first and last
- * buckets towards the center. Each cursor has a pointer to a bucket and a count
- * of the numbers processed by the procedure. At each step, we add the smaller
- * bucket's population to each count, and move the bucket pointers inward when
- * all the bucket's numbers are consumed. We do this until either the two
- * cursors point at the same bucket. At this point, if the bucket is small
- * enough, we sort that bucket's contents, and use the cursor counts to
- * determine the median. A special case exists when one cursor is pointing to
- * the max value in one bucket, and the next cursor is pointing to the minimum
- * value in the adjacent bucket. The median is then the average of one bucket's
- * min() and the other bucket's max().
- * 
- * If the above procedure reaches a bucket whose size is large enough, then
- * instead of sorting the bucket, we split the bucket by partitioning its
- * numbers based on the average of the bucket's min and max values (if min =
- * max, then all the numbers in the bucket are identical, and no splitting or
- * sorting is required). We replace the original bucket with the two smaller
- * buckets, and repeat the FindMedian() procedure.
- * 
- * The expensive sorting step in FindMedian() can be optimized by keeping track
- * of how many of a bucket's numbers are already sorted. When adding new numbers
- * to an already-sorted bucket, the numbers are appended to the end of the
- * array. When sorting is next required, we sort only the range of new numbers
- * (at the end of the array), then perform a linear, in-space merge of the two
- * subarrays.
- * 
- * 
- * $$O(n log n)$$ in the worst case, due to the bucket sorting. (The linear scan
- * of buckets in AddNum() might imply a running time of $$O(n^2)$$, but this
- * could only happen if the bucket count was a significant fraction of $$n$$,
- * which is not the case: buckets are only created when a bucket gets large,
- * which implies the bucket count is bounded by $$O(log n)$$).
- * 
- */
 public class SerializeAndDeserializeBinaryTree extends LeetCode {
 
   public static void main(String[] args) {
@@ -75,13 +14,14 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
 
   public void run() {
 
-    // x("3[50[-3000[|]|5[|]]|7[|]]");
-     var tree = t(3, t(5, null, t(2)), t(7, t(-3000, t(4), t(-6))));
-   // var tree = t(1, null, t(2, t(3), null));
-    var c = new Codec();
+    var tree = t(3, t(5, null, t(2)), t(7, t(-1000, t(4), t(-6))));
+    var c = new CodecLC();
     var s = c.serialize(tree);
     pr(s);
     TreeNode tout = c.deserialize(s);
+    pr("tree in :", tree);
+    pr("tree out:", tout);
+
     verify(tree.toString(), tout.toString());
   }
 
@@ -133,44 +73,271 @@ public class SerializeAndDeserializeBinaryTree extends LeetCode {
 
   public class Codec {
 
+    private static final String dig = "012345abcdefghijklmnopqrstuvwxyz";
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-
+      db("serialize:", root);
       List<TreeNode> frontier = new ArrayList<>();
       frontier.add(root);
-      StringBuilder sb = new StringBuilder("[");
+      StringBuilder sb = new StringBuilder();
       int cursor = 0;
       while (cursor < frontier.size()) {
+        db("...cursor:", cursor, "string:", quote(sb.toString()));
         var n = frontier.get(cursor++);
-        pr("popped tree:");
-        if (n != null) {
-          pr("....val:", n.val, "left:", n.left != null, "right:", n.right != null);
-        }
+        db("...cursor:", cursor, "tree:", n);
         if (n == null) {
-          sb.append("null,");
+          sb.append(".");
         } else {
-          sb.append(n.val);
-          sb.append(',');
+          {
+            var x = n.val;
+            if (x < 0) {
+              sb.append('-');
+              x = -x;
+            } else
+              sb.append('+');
+
+            char dig1 = dig.charAt(x & 0x1f);
+            char dig2 = dig.charAt(x >> 5);
+            if (dig2 != '0')
+              sb.append(dig2);
+            sb.append(dig1);
+          }
           frontier.add(n.left);
           frontier.add(n.right);
         }
       }
-      if (true) {
-        int i = sb.length() - 5;
-        while (i >= 0 && sb.subSequence(i, i + 5).equals("null,")) {
-          sb.setLength(i);
-          i -= 5;
-        }
-        if (sb.charAt(sb.length() - 1) == ',')
-          sb.setLength(sb.length() - 1);
-      }
-      sb.append(']');
+      trimSuffix(sb, ".");
+      //      if (true) {
+      //        int newLen = sb.length();
+      //        int i = newLen - 1;
+      //        while (i >= 0 && sb.charAt(i) == '.') {
+      //          newLen = i;
+      //          i--;
+      //        }
+      //        sb.setLength(newLen);
+      //      }
       return sb.toString();
+    }
+
+    private int cursor;
+    private String text;
+
+    private boolean hasMore() {
+      return cursor < text.length();
+    }
+
+    private char peek() {
+      if (hasMore())
+        return text.charAt(cursor);
+      return 0;
+    }
+
+    private char read() {
+      char c = peek();
+      cursor++;
+      return c;
+    }
+
+    private boolean readIf(char c2) {
+      if (peek() == c2) {
+        cursor++;
+        return true;
+      }
+      return false;
+    }
+
+    private char read(char c2) {
+      if (peek() != c2) {
+        throw new IllegalArgumentException();
+      }
+      return read();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-      return new TreeNode(0, null, null);
+      db("deserialize:", quote(data));
+
+      List<Integer> values = new ArrayList<>();
+      cursor = 0;
+      text = data;
+      while (hasMore()) {
+        db("...peek:", Character.toString(peek()));
+        Integer value = null;
+
+        if (!readIf('.')) {
+          boolean neg = readIf('-');
+          if (!neg)
+            read('+');
+
+          char d = read();
+          int slot2 = dig.indexOf(d);
+          checkState(slot2 >= 0);
+          char p = peek();
+          int slot1 = dig.indexOf(p);
+          if (slot1 >= 0)
+            read();
+          else {
+            slot1 = slot2;
+            slot2 = 0;
+          }
+          var val = (slot2 << 5) | slot1;
+          if (neg)
+            val = -val;
+          value = val;
+        }
+        db("...adding value:", value);
+        values.add(value);
+      }
+
+      TreeNode result = parse(values, 0);
+      return result;
+    }
+
+    private TreeNode parse(List<Integer> values, int cursor) {
+      if (cursor >= values.size())
+        return null;
+      Integer val = values.get(cursor++);
+      if (val == null)
+        return null;
+      return new TreeNode(val, parse(values, cursor + 1), parse(values, cursor + 1));
     }
   }
+
+  private static void trimSuffix(StringBuilder sb, String suffix) {
+    while (true) {
+      int x = sb.length() - suffix.length();
+      if (x < 0)
+        break;
+      if (!sb.substring(x).equals(suffix))
+        break;
+      sb.setLength(x);
+    }
+  }
+
+  public class CodecLC extends Codec {
+
+    public String serialize(TreeNode root) {
+      db("serialize:", root);
+      StringBuilder sb = new StringBuilder();
+      if (root == null) {
+        sb.append("null");
+      } else {
+        sb.append('[');
+        List<TreeNode> frontier = new ArrayList<>();
+        frontier.add(root);
+        int cursor = 0;
+        while (cursor < frontier.size()) {
+          db("...cursor:", cursor, "string:", quote(sb.toString()));
+          var n = frontier.get(cursor++);
+          db("...cursor:", cursor, "tree:", n);
+          if (sb.length() > 1) // && sb.charAt(sb.length() - 1) != '[')
+            sb.append(',');
+          if (n == null) {
+            sb.append("null");
+          } else {
+            sb.append(n.val);
+            frontier.add(n.left);
+            frontier.add(n.right);
+          }
+        }
+        trimSuffix(sb, ",null");
+        sb.append(']');
+      }
+      return sb.toString();
+    }
+
+    private int cursor;
+    private String text;
+
+    private boolean hasMore() {
+      return cursor < text.length();
+    }
+
+    private char peek() {
+      if (hasMore())
+        return text.charAt(cursor);
+      return 0;
+    }
+
+    private char read() {
+      char c = peek();
+      cursor++;
+      return c;
+    }
+
+    private boolean readIf(char c2) {
+      if (peek() == c2) {
+        cursor++;
+        return true;
+      }
+      return false;
+    }
+
+    private boolean readIf(String s) {
+      var cnew = cursor + s.length();
+      if (cnew <= text.length() && text.substring(cursor, cnew).equals(s)) {
+        cursor = cnew;
+        return true;
+      }
+      return false;
+    }
+
+    private char read(char c2) {
+      if (peek() != c2) {
+        throw new IllegalArgumentException();
+      }
+      return read();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+      db("deserialize:", quote(data));
+      TreeNode result = null;
+      if (!readIf("null")) {
+        read('[');
+
+        List<Integer> values = new ArrayList<>();
+        cursor = 0;
+        text = data;
+        while (hasMore()) {
+          db("...peek:", Character.toString(peek()));
+          Integer value = null;
+          if (!readIf("null")) {
+
+            boolean neg = readIf('-');
+            int i = 0;
+            int oldCursor = cursor;
+            while (true) {
+              int digit = peek() - '0';
+              if (digit < 0 || digit > 9)
+                break;
+              read();
+              i = i * 10 + digit;
+            }
+            if (cursor == oldCursor)
+              throw new IllegalArgumentException();
+            value = i;
+          }
+          db("...adding value:", value);
+          values.add(value);
+        }
+
+        result = parse(values, 0);
+        read(']');
+      }
+      checkState(cursor == text.length(), "extra characters");
+      return result;
+    }
+
+    private TreeNode parse(List<Integer> values, int cursor) {
+      if (cursor >= values.size())
+        return null;
+      Integer val = values.get(cursor++);
+      if (val == null)
+        return null;
+      return new TreeNode(val, parse(values, cursor + 1), parse(values, cursor + 1));
+    }
+  }
+
 }
