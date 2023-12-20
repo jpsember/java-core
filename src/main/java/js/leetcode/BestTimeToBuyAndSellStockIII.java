2bdfed1b3d03ab5be147d77a63556f0919747f80
Jeff Sember <jpsember@gmail.com>
2023-12-19 17:39:46 -0800

    Issue #23

--- src/main/java/js/leetcode/BestTimeToBuyAndSellStockIII.java
+++ src/main/java/js/leetcode/BestTimeToBuyAndSellStockIII.java
@@ -46,36 +46,53 @@ public class BestTimeToBuyAndSellStockIII extends LeetCode {
 
     int bestProfit = 0;
     for (int i = 1; i < n; i++) {
-      var profit = fwd[i].maxProfit + bwd[n - 1 - i].maxProfit;
+      var profit = fwd[i * 2 + 0] + bwd[(n - 1 - i) * 2 + 0];
       bestProfit = Math.max(profit, bestProfit);
+      //      var profit = fwd[i].maxProfit + bwd[n - 1 - i].maxProfit;
+      //      bestProfit = Math.max(profit, bestProfit);
     }
     return bestProfit;
   }
 
-  private static class Info {
-    int minPrice; // minimum price seen so far
-    int maxProfit; // maximal (nonnegative) profit seen so far
-  }
+  //  private static class Info {
+  //    int minPrice; // minimum price seen so far
+  //    int maxProfit; // maximal (nonnegative) profit seen so far
+  //  }
 
-  private Info[] calcInfo(int[] prices) {
-    var result = new Info[prices.length];
+  private int[] calcInfo(int[] prices) {
+    //  private Info[] calcInfo(int[] prices) {
+    //    var result = new Info[prices.length];
+    var result = new int[prices.length * 2];
 
-    Info prevInfo = new Info();
-    prevInfo.minPrice = prices[0];
+    //
+    //    Info prevInfo = new Info();
+    //    prevInfo.minPrice = prices[0];
+    //
+    var prevMinPrice = prices[0];
+    var prevMaxProfit = 0;
 
     for (int i = 0; i < prices.length; i++) {
       var price = prices[i];
-      var info = new Info();
+      //      var info = new Info();
+      //
+      var maxProfit = Math.max(prevMaxProfit, price - prevMinPrice);
 
-      // See if selling at this price improves the best profit
-      info.maxProfit = Math.max(prevInfo.maxProfit, price - prevInfo.minPrice);
-      info.minPrice = prevInfo.minPrice;
+      //      // See if selling at this price improves the best profit
+      //      info.maxProfit = Math.max(prevInfo.maxProfit, price - prevInfo.minPrice);
+      //      info.minPrice = prevInfo.minPrice;
+      //
+      var minPrice = Math.min(price, prevMinPrice);
 
-      if (price < info.minPrice)
-        info.minPrice = price;
+      //      if (price < info.minPrice)
+      //        info.minPrice = price;
+      //
+      prevMinPrice = minPrice;
+      prevMaxProfit = maxProfit;
 
-      prevInfo = info;
-      result[i] = info;
+      // result[i * 2 + 0] = prevMinPrice;
+      result[i * 2 + 0] = prevMaxProfit;
+      //      prevInfo = info;
+      //      result[i] = info;
     }
     return result;
   }
