e0dbec878d2f52d216d75b6ebbe15a09f6cc63aa
Jeff Sember <jpsember@gmail.com>
2023-12-29 12:45:23 -0800

    Issue #23

--- src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
+++ src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
@@ -2,6 +2,9 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class LongestIncreasingPathInAMatrix extends LeetCode {
 
   public static void main(String[] args) {
@@ -22,7 +25,39 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
 
   // ------------------------------------------------------------------
 
+  private class Pt implements Comparator {
+    int x;
+    int y;
+    int value;
+    
+    Pt(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int flag() {
+      return visitFlags[y][x];
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+      var p1 = (Pt) o1;
+      var p2 = (Pt) o2;
+      int diff = 
+    }
+  }
+
   public int longestIncreasingPath(int[][] matrix) {
+    int w = matrix[0].length;
+    int h = matrix.length;
+    visitFlags = new int[h][w];
+    this.matrix = matrix;
+
+    var queue = new PriorityQueue<Pt>(w * h, new Pt(0,0));
     return -1;
   }
+
+  private int[][] visitFlags;
+  private int[][] matrix;
+
 }
