818733c39cca0f6f8bf6081da9ef2c4f26ee0af6
Jeff Sember <jpsember@gmail.com>
2023-12-29 13:22:08 -0800

    Issue #23

--- src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
+++ src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
@@ -2,8 +2,8 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LongestIncreasingPathInAMatrix extends LeetCode {
 
@@ -13,6 +13,7 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
 
   public void run() {
     x("[[9,9,4],[6,6,8],[2,1,1]]", 4);
+    x("[[3,4,5],[3,2,6],[2,2,1]]", 4);
   }
 
   private void x(String s, int expected) {
@@ -25,39 +26,103 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
 
   // ------------------------------------------------------------------
 
-  private class Pt implements Comparator {
-    int x;
-    int y;
-    int value;
-    
-    Pt(int x, int y) {
+  private class State {
+
+    State(int x, int y, int pathLength) {
       this.x = x;
       this.y = y;
+      this.pathLength = pathLength;
     }
 
-    public int flag() {
-      return visitFlags[y][x];
+    public int value() {
+      return matrix[y][x];
     }
 
     @Override
-    public int compare(Object o1, Object o2) {
-      var p1 = (Pt) o1;
-      var p2 = (Pt) o2;
-      int diff = 
+    public String toString() {
+      return "(" + x + " " + y + " len:" + pathLength + " val:" + value() + ")";
     }
+
+    final int x;
+    final int y;
+    final int pathLength;
+  }
+
+  private int compareStates(State a, State b) {
+    var result = a.value() - b.value();
+    if (result == 0)
+      result = a.y - b.y;
+    if (result == 0)
+      result = a.x - b.x;
+    return result;
   }
 
   public int longestIncreasingPath(int[][] matrix) {
-    int w = matrix[0].length;
-    int h = matrix.length;
-    visitFlags = new int[h][w];
+    db = true;
+
+    matrixWidth = matrix[0].length;
+    matrixHeight = matrix.length;
+    visitFlags = new int[matrixHeight][matrixWidth];
     this.matrix = matrix;
 
-    var queue = new PriorityQueue<Pt>(w * h, new Pt(0,0));
-    return -1;
+    List<State> stack = new ArrayList<>();
+
+    for (int y = matrixHeight - 1; y >= 0; y--) {
+      for (int x = matrixWidth - 1; x >= 0; x--) {
+        stack.add(new State(x, y, 1));
+      }
+    }
+    // Sort so higher priority states are at the END of the stack, so
+    // we perform a depth-first search
+    stack.sort((a, b) -> compareStates(b, a));
+
+    int maxLen = 0;
+
+    while (!stack.isEmpty()) {
+      var s = stack.remove(stack.size() - 1);
+      maxLen = Math.max(maxLen, s.pathLength);
+      db(VERT_SP, "popped state:", s);
+
+      var oldPathLength = visitFlags[s.y][s.x];
+      if (oldPathLength >= s.pathLength)
+        break;
+      visitFlags[s.y][s.x] = s.pathLength;
+
+      // Expand search to neighbors with greater values 
+      todo("search lower values first");
+      nbrs.clear();
+      searchNeighbor(s, -1, 0);
+      searchNeighbor(s, 1, 0);
+      searchNeighbor(s, 0, -1);
+      searchNeighbor(s, 0, 1);
+      if (nbrs.size() >= 2)
+        nbrs.sort((a, b) -> compareStates(b, a));
+      stack.addAll(nbrs);
+    }
+    return maxLen;
   }
 
+  private void searchNeighbor(State s, int xm, int ym) {
+    int nx = s.x + xm;
+    if (nx < 0 || nx >= matrixWidth)
+      return;
+
+    int ny = s.y + ym;
+    if (ny < 0 || ny >= matrixHeight)
+      return;
+    db("...expand to:", nx, ny, "? value", matrix[ny][nx]);
+    if (matrix[ny][nx] <= s.value()) {
+      db("......no, value isn't higher");
+      return;
+    }
+    var newState = new State(nx, ny, s.pathLength + 1);
+    db("......yes, pushing", newState);
+    nbrs.add(newState);
+  }
+
+  private List<State> nbrs = new ArrayList<>(4);
   private int[][] visitFlags;
   private int[][] matrix;
-
+  private int matrixWidth;
+  private int matrixHeight;
 }
