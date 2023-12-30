fca65d9c15c5e6872644e79b40c8aa998ef80b81
Jeff Sember <jpsember@gmail.com>
2023-12-30 12:43:36 -0800

    Issue #23

--- src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
+++ src/main/java/js/leetcode/LongestIncreasingPathInAMatrix.java
@@ -23,22 +23,22 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
     x("[[13,6,16,6,16,4],[9,13,5,13,7,11],[11,7,9,17,0,7],[7,8,5,14,11,8],[14,2,8,7,9,5],[1,15,3,11,11,6]]",
         6, -1);
 
-    if (false) {
+    if (true) {
       x(200, 200, 1965);
-      return;
+      //return;
     }
 
-    //    x("[12,11,7],[2,3,14]]", 3, -1);
-    //
-    //    x("[12,11,7,12,2],[2,3,4,1,13]]", 5, -1);
-    //
-    //    x("[[1,6,12,1,3],[8,4,6,10,5],[12,11,7,12,2],[2,3,4,1,13],[14,6,0,14,13]]", 5, -1);
+    x("[12,11,7],[2,3,14]]", 3, -1);
+
+    x("[12,11,7,12,2],[2,3,4,1,13]]", 5, -1);
+
+    x("[[1,6,12,1,3],[8,4,6,10,5],[12,11,7,12,2],[2,3,4,1,13],[14,6,0,14,13]]", 5, -1);
 
     x("[[0,1,2,3,4,5,6],[7,8,9,10,11,12,13],[14,15,16,17,18,19,20],[21,22,23,24,25,26,27],[28,29,30,31,32,33,34],[35,36,37,38,39,40,41],[42,43,44,45,46,47,48]]",
         7, -1);
 
-    // x("[[9,9,4],[6,6,8],[2,1,1]]", 3, 4);
-    // x("[[3,4,5],[3,2,6],[2,2,1]]", 3, 4);
+    x("[[9,9,4],[6,6,8],[2,1,1]]", 3, 4);
+    x("[[3,4,5],[3,2,6],[2,2,1]]", 3, 4);
   }
 
   private void x(int width, int height, int seed) {
@@ -92,7 +92,9 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
     var origin = rowOffset + 1;
     var cursorEnd = a.length - rowOffset;
 
-    final int qlen = width * height * 2; // This could be decreased I suppose
+    // The maximum frontier size is the number of cells in the matrix, in the case
+    // where they are all the same value and thus have no incoming edges.
+    final int qlen = width * height;
     int[] q1 = new int[qlen];
     int[] q2 = new int[qlen];
     int c1 = 0;
@@ -117,26 +119,21 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
             count++;
         }
         inEdgeCount[cursor] = (byte) count;
-        if (count == 0) {
-          db("adding seed point", pt(cursor));
+        if (count == 0)
           q1[c1++] = cursor;
-        }
       }
     }
 
     // Keep advancing a step in the BFS until the horizon is empty.
     // The longest path length is the number of iterations.
 
-    int maxQueueSize = 0;
     int len = 0;
     while (c1 != 0) {
       len++;
-      if (db)
-        db(VERT_SP, "length:", len, "horizon:", darray(q1, c1));
-      c2 = 0;
       if (db) {
-        maxQueueSize = Math.max(maxQueueSize, c1);
+        db(VERT_SP, "length:", len, "horizon:", darray(q1, c1));
       }
+      c2 = 0;
 
       while (c1-- != 0) {
         var ca = q1[c1];
@@ -162,8 +159,6 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
       q1 = tmp;
       c1 = c2;
     }
-
-    db("maxQueueSize:", maxQueueSize);
     return len;
   }
 
@@ -178,9 +173,6 @@ public class LongestIncreasingPathInAMatrix extends LeetCode {
       System.arraycopy(row, 0, res, cursor, w - 1);
       cursor += w;
     }
-    //    pr("w:",matrix[0].length,"h:",matrix.length);
-    //    pr("padd:",INDENT,res,res.length);
-    //    halt();
     return res;
   }
 
