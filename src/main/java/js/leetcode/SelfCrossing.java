5bb0f1e4eec37153b887e1048a1cb0f00e4ca2e0
Jeff Sember <jpsember@gmail.com>
2023-12-30 15:40:00 -0800

    Issue #23

--- src/main/java/js/leetcode/SelfCrossing.java
+++ src/main/java/js/leetcode/SelfCrossing.java
@@ -32,40 +32,29 @@ public class SelfCrossing extends LeetCode {
 
   }
 
-  private String dumpSegs(int[] segs, int nseg) {
-    var js = list();
-    for (int i = 0; i < nseg; i++)
-      js.add(new Pt(segs[i * 2], segs[i * 2 + 1]).toString());
-    return js.prettyPrint();
-  }
+  // ------------------------------------------------------------------
 
   public boolean isSelfCrossing(int[] distance) {
     final int MAX_SEGS = 6;
     // One additional point for each segment, including initial starting point
-    int[] pts = new int[(MAX_SEGS + 1) * 2];
+    int[] pts = new int[(distance.length + 1) * 2];
 
     // Add an origin 0,0
-    int npts = addSeg(pts, 0, 0, 0);
+    int npts = addPoint(pts, 0, 0, 0);
 
     // To deal with the special case of a segment intersecting its (parallel) segment
     // 4 segments back (with no 5th segment), add a zero-length segment immediately
     // to act as this missing 5th segment
-    npts = addSeg(pts, npts, 0, 0);
+    npts = addPoint(pts, npts, 0, 0);
 
     for (int d : distance) {
-      db(VERT_SP, "distance:", d, INDENT, dumpSegs(pts, npts));
       var nx = 0;
       var ny = d;
-
-      npts = addSeg(pts, npts, nx, ny);
-      db("after adding segment:", dumpSegs(pts, npts));
-
+      npts = addPoint(pts, npts, nx, ny);
       if (npts >= 5) {
-        db("...checking for intersection with -5");
         if (segsIntersect(pts, npts - 2, npts - 5))
           return true;
         if (npts >= 7) {
-          db("...checking for intersection with -7");
           if (segsIntersect(pts, npts - 2, npts - 7))
             return true;
         }
@@ -94,105 +83,25 @@ public class SelfCrossing extends LeetCode {
   }
 
   private boolean segsIntersect(int[] pts, int segA, int segB) {
-    // We can assume s is horizontal and we are vertical
-
-    db("segsIntersect a:", segA, "b:", segB, pts);
+    // We can assume A is vertical and B is horizontal 
 
     int p0x = pts[segA * 2];
     int p0y = pts[segA * 2 + 1];
-    int p1y = pts[(segA+1) * 2 + 1];
+    int p1y = pts[segA * 2 + 3];
 
-    db("...A:",p0x,p0y,"...",p0x,p1y);
-    
     int sx0 = pts[segB * 2] - p0x;
-    int sx1 = pts[(segB + 1) * 2] - p0y;
     int sy0 = pts[segB * 2 + 1] - p0y;
-    db("...B:",sx0,sy0,"...",sx1,sy0);
-    
+    int sx1 = pts[segB * 2 + 2] - p0y;
+
     if (Math.max(sx0, sx1) < 0 || Math.min(sx0, sx1) > 0)
       return false;
-    if (sy0 < 0 || sy0 > p1y)
-      return false;
-    return true;
+    return !(sy0 < 0 || sy0 > p1y);
   }
 
-  private int addSeg(int[] segs, int nseg, int x1, int y1) {
+  private int addPoint(int[] segs, int nseg, int x1, int y1) {
     segs[nseg * 2] = x1;
     segs[nseg * 2 + 1] = y1;
     return nseg + 1;
-  }
-
-  private class Pt {
-    final int x;
-    final int y;
-
-    Pt(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-
-    Pt rot(int dir) {
-      // We actually only need to rotate by -90 degrees
-      checkState(dir == -1);
-      dir &= 3;
-      switch (dir) {
-      default:
-        return this;
-      case 1:
-        return new Pt(-y, x);
-      case 2:
-        return new Pt(-x, -y);
-      case 3:
-        return new Pt(y, -x);
-      }
-    }
-
-    Pt translate(int dx, int dy) {
-      return new Pt(x + dx, y + dy);
-    }
-
-    @Override
-    public String toString() {
-      return "(" + x + " " + y + ")";
-    }
-  }
-
-  private class Seg {
-    public final Pt p0, p1;
-
-    Seg(Pt p0, Pt p1) {
-      this.p0 = p0;
-      this.p1 = p1;
-    }
-
-    Seg rotate(int dir) {
-      return new Seg(p0.rot(dir), p1.rot(dir));
-    }
-
-    Seg translate(int dx, int dy) {
-      return new Seg(p0.translate(dx, dy), p1.translate(dx, dy));
-    }
-
-    boolean intersects(Seg s) {
-      // We can assume s is horizontal and we are vertical
-      checkState(p0.x == p1.x);
-      checkState(p1.y >= p0.y);
-      int sx0 = s.p0.x - p0.x;
-      int sx1 = s.p1.x - p0.x;
-      checkState(s.p0.y == s.p1.y);
-      int sy0 = s.p0.y - p0.y;
-      if (Math.max(sx0, sx1) < 0 || Math.min(sx0, sx1) > 0)
-        return false;
-      if (sy0 < 0 || sy0 > p1.y)
-        return false;
-      return true;
-    }
-
-    @Override
-    public String toString() {
-      return "[" + p0 + "..." + p1 + "]";
-    }
-
   }
 
 }
