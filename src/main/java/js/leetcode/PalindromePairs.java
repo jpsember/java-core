9055f4144e0c254d5725093b41760d608824ca24
Jeff Sember <jpsember@gmail.com>
2023-12-30 19:27:55 -0800

    Issue #23

--- src/main/java/js/leetcode/PalindromePairs.java
+++ src/main/java/js/leetcode/PalindromePairs.java
@@ -8,9 +8,7 @@ import java.util.List;
 import js.json.JSList;
 
 /**
- * I think this is easy? I guess I must be wrong.
- * 
- * Actually, it is easy, once I dealt with a degenerate case.
+ * I think I need to use a trie data structure
  */
 public class PalindromePairs extends LeetCode {
 
@@ -19,9 +17,7 @@ public class PalindromePairs extends LeetCode {
   }
 
   public void run() {
-
     x("[\"abcd\",\"dcba\",\"lls\",\"s\",\"sssll\"]", "[[0,1],[1,0],[3,2],[2,4]]");
-
   }
 
   private void x(String s, String sExp) {
@@ -43,11 +39,112 @@ public class PalindromePairs extends LeetCode {
   // ------------------------------------------------------------------
 
   public List<List<Integer>> palindromePairs(String[] words) {
-    var a = new ArrayList<List<Integer>>();
-    var b = new ArrayList<Integer>();
-    b.add(7);
-    b.add(8);
-    a.add(b);
-    return a;
+    var result = new ArrayList<List<Integer>>();
+
+    var fwdTrie = new Trie();
+    for (int i = 0; i < words.length; i++) {
+      fwdTrie.add(words[i], i);
+    }
+
+    pr(fwdTrie);
+
+    int wordNumber = -1;
+    for (var w : words) {
+      wordNumber++;
+      int i = w.length();
+      var t = fwdTrie;
+      int lastWord = -1;
+      while (i >= 0) {
+        if (t.index >= 0) {
+          lastWord = t.index;
+          var prefix = w.substring(0, i);
+          pr("found prefix at i:", i, quote(prefix));
+          if (isPal(prefix)) {
+            var res = new ArrayList<Integer>(2);
+            res.add(lastWord);
+            res.add(wordNumber);
+            result.add(res);
+          }
+        }
+        if (i == 0)
+          break;
+        i--;
+        var x = w.charAt(i);
+        t = t.child(x);
+        if (t == null)
+          break;
+      }
+
+    }
+
+    return result;
   }
+
+  private static boolean isPal(String s) {
+    int i = s.length();
+    int j = 0;
+    while (i > j) {
+      if (s.charAt(i - 1) != s.charAt(j))
+        return false;
+      i--;
+      j++;
+    }
+    return true;
+  }
+
+  private class Trie {
+
+    public Trie child(char c) {
+      int i = c - 'a';
+      return children[i];
+    }
+
+    public void add(String word, int index) {
+      var node = this;
+      for (int i = 0; i < word.length(); i++) {
+        int c = word.charAt(i) - 'a';
+        var child = node.children[c];
+        if (child == null) {
+          child = new Trie();
+          node.children[c] = child;
+        }
+        node = child;
+      }
+      node.index = index;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      aux(sb, 2);
+      return sb.toString();
+    }
+
+    private void aux(StringBuilder sb, int indent) {
+      if (index >= 0) {
+        sb.append('*');
+        sb.append(index);
+      }
+      boolean anyChild = false;
+      for (int i = 0; i < 26; i++) {
+        var child = children[i];
+        if (child == null)
+          continue;
+        if (!anyChild) {
+          sb.append("[");
+          anyChild = true;
+        }
+        sb.append('\n');
+        sb.append(spaces(indent));
+        sb.append((char) (i + 'a'));
+        child.aux(sb, indent + 2);
+      }
+      if (anyChild)
+        sb.append("]");
+    }
+
+    private Trie[] children = new Trie[26];
+    private int index = -1;
+  }
+
 }
