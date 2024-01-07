fae14dfd21cc00f3ef9c477e45314ec9ea23bd5b
Jeff Sember <jpsember@gmail.com>
2024-01-07 11:08:18 -0800

    Issue #23

--- src/main/java/js/leetcode/LongestIncreasingSubsequence.java
+++ src/main/java/js/leetcode/LongestIncreasingSubsequence.java
@@ -4,6 +4,7 @@ import static js.base.Tools.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * I think this is a dynamic programming problem, working from the end of the
@@ -23,8 +24,10 @@ public class LongestIncreasingSubsequence extends LeetCode {
   }
 
   public void run() {
-    x("[10,9,2,5,3,7,101,18]");
+    x("[1,3,6,7,9,4,10,5,6]");
+
     x("[0,1,0,3,2,3]");
+    x("[10,9,2,5,3,7,101,18]");
     x("[7,7,7,7,7,7,7]");
     int[] n = new int[2500];
     for (int i = 0; i < n.length; i++)
@@ -43,6 +46,44 @@ public class LongestIncreasingSubsequence extends LeetCode {
     if (expected < 0)
       expected = new SLOW().lengthOfLIS(nums);
     verify(res, expected);
+  }
+
+  //------------------------------
+
+  public int NEWlengthOfLIS(int[] nums) {
+    // This is a map whose key is a nums[] element, and the value is the 
+    // length of the longest subsequence whose first element is that element
+    // 
+    var bestResultsMap = new TreeMap<Integer, Integer>();
+    pr(nums);
+    for (int i = nums.length - 1; i >= 0; i--) {
+      int val = nums[i];
+      pr(VERT_SP, "val:", val);
+
+      // Let len0 length of longest subsequence in map whose first value = val (or 0)
+      // Let len1 be key in map of longest subsequence whose first value > val (or 0)
+      // Store (val, max(len0, 1+len1))
+      int len0 = 0;
+      int len1 = 0;
+
+      len0 = bestResultsMap.getOrDefault(val, 0);
+
+      var query = bestResultsMap.tailMap(val + 1);
+      if (!query.isEmpty()) {
+        len1 = query.get(query.firstKey());
+      }
+
+      var len = Math.max(len0, 1 + len1);
+
+      pr("storing", val, len);
+      // Store our result in the map
+      bestResultsMap.put(val, len);
+
+    }
+    int best = 0;
+    for (var x : bestResultsMap.values())
+      best = x > best ? x : best;
+    return best;
   }
 
   private class SLOW {
@@ -81,31 +122,30 @@ public class LongestIncreasingSubsequence extends LeetCode {
 
   public int lengthOfLIS(int[] nums) {
     final int nn = nums.length;
-
-    // We reuse the nums[] array to store the longest length in the upper 16 bits.
+    
+    // Length of longest subsequence whose last element is nums[i]
+    var tbl = new int[nn];
 
     for (int i = 0; i < nn; i++) {
-      // Make all numbers positive (as we process them) to avoid problems with sign bit interfering with upper bits
-      nums[i] += 10000;
+
       // Choose longest compatible subsequence before i to concatenate with nums[i]
       // ...this will produce an n^2 algorithm
       int val = nums[i];
       int c = 0;
 
       for (int j = 0; j < i; j++) {
-        var numj = (nums[j] & 0xffff);
+        var numj = nums[j];
         if (numj < val) {
-          var tblVal = nums[j] >> 16;
+          var tblVal = tbl[j];
           if (tblVal > c)
             c = tblVal;
         }
       }
-      nums[i] |= (1 + c) << 16;
+      tbl[i] = 1 + c;
     }
     var max = 0;
-    for (var x : nums) {
-      var len = x >> 16;
-      max = max < len ? len : max;
+    for (var x : tbl) {
+      max = max < x ? x : max;
     }
     return max;
   }
