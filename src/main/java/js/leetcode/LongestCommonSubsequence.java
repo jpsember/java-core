7d61e0a92a6a8ccf99b790706d518ad68e9bc4f1
Jeff Sember <jpsember@gmail.com>
2024-01-14 15:18:42 -0800

    Issue #23: added recursion with memo

--- src/main/java/js/leetcode/LongestCommonSubsequence.java
+++ src/main/java/js/leetcode/LongestCommonSubsequence.java
@@ -2,6 +2,9 @@ package js.leetcode;
 
 import static js.base.Tools.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestCommonSubsequence extends LeetCode {
 
   public static void main(String[] args) {
@@ -10,13 +13,8 @@ public class LongestCommonSubsequence extends LeetCode {
 
   public void run() {
     loadTools();
-    //    db = true;
-
-    // x("zbbddcc", "aaabbcc");
-
-    // x("abcde", "ace", 3);
-    x(1965, 30, 7);
-    x(1966, 50, 14);
+    x("abcde", "ace");
+    x(1966, 1000, 316);
   }
 
   private void x(String a, String b) {
@@ -39,7 +37,7 @@ public class LongestCommonSubsequence extends LeetCode {
     checkpoint("Done");
     pr(INDENT, res);
     if (expected < 0) {
-      expected = new Recursion2().longestCommonSubsequence(ca, cb);
+      expected = new Recursion().longestCommonSubsequence(ca, cb);
       //      var exp2 = new RecursionNaive().longestCommonSubsequence(ca, cb);
       //      checkState(exp2 == expected);
     }
@@ -55,7 +53,6 @@ public class LongestCommonSubsequence extends LeetCode {
     var a = randString(length);
     var b = randString(length);
     x(a, b, expected);
-
   }
 
   private String randString(int length) {
@@ -80,10 +77,16 @@ public class LongestCommonSubsequence extends LeetCode {
   public int longestCommonSubsequence(String text1, String text2) {
     var ca = text1.toCharArray();
     var cb = text2.toCharArray();
-    return new Recursion2().longestCommonSubsequence(ca, cb);
+    return new RecursionWithMemo().longestCommonSubsequence(ca, cb);
   }
 
-  private class RecursionNaive implements Alg {
+  private int find(char[] array, int cursor, char seekChar) {
+    while (cursor < array.length && array[cursor] != seekChar)
+      cursor++;
+    return cursor;
+  }
+
+  class RecursionNaive implements Alg {
     @Override
     public int longestCommonSubsequence(char[] a, char[] b) {
 
@@ -108,7 +111,7 @@ public class LongestCommonSubsequence extends LeetCode {
     }
   }
 
-  private class Recursion2 implements Alg {
+  class Recursion implements Alg {
     @Override
     public int longestCommonSubsequence(char[] a, char[] b) {
       return aux(a, 0, b, 0);
@@ -152,11 +155,65 @@ public class LongestCommonSubsequence extends LeetCode {
       return result;
     }
 
-    private int find(char[] array, int cursor, char seekChar) {
-      while (cursor < array.length && array[cursor] != seekChar)
-        cursor++;
-      return cursor;
-    }
   }
 
+  class RecursionWithMemo implements Alg {
+
+    @Override
+    public int longestCommonSubsequence(char[] a, char[] b) {
+      mMemo.clear();
+      return aux(a, 0, b, 0);
+    }
+
+    private Map<Integer, Integer> mMemo = new HashMap<>();
+
+    private int aux(char[] a, int aCursor, char[] b, int bCursor) {
+      // String length is at most 1000, which fits within 10 bits
+      var key = (aCursor << 10) | bCursor;
+      var result = mMemo.getOrDefault(key, -1).intValue();
+      if (result >= 0)
+        return result;
+
+      if (db) {
+        pushIndent(2);
+        db("aux", dbstr(a, aCursor), dbstr(b, bCursor));
+      }
+      do {
+        // Base cases:
+        //
+        // If length of either string is zero, answer is zero
+        // 
+        // Recursive case:
+        //
+        // If first chars are the same, answer is 1 plus the LCS of the suffixes.
+        // Otherwise, we either use the first character of A, 
+        // use the 
+        // Otherwise, the answer is greater of LCS(a+1,b+1), LCS(a, b'), LCS(a',b),
+        // where a' is the smallest value that satisfies   A[a'>a] == B[b] (or A.length if not found).
+        // In other words, look for the first appearance of A's character in B (and vice versa).
+        //
+        if (aCursor == a.length || bCursor == b.length) {
+          result = 0;
+          break;
+        }
+        if (a[aCursor] == b[bCursor]) {
+          result = 1 + aux(a, aCursor + 1, b, bCursor + 1);
+        } else {
+          int ap = find(a, aCursor + 1, b[bCursor]);
+          int bp = find(b, bCursor + 1, a[aCursor]);
+          result = Math.max(aux(a, ap, b, bCursor), aux(a, aCursor, b, bp));
+          if (aCursor + 1 != ap || bCursor + 1 != bp)
+            result = Math.max(result, aux(a, aCursor + 1, b, bCursor + 1));
+        }
+      } while (false);
+      mMemo.put(key, result);
+
+      if (db) {
+        db("...result:", result);
+        popIndent();
+      }
+      return result;
+    }
+
+  }
 }
