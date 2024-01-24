4aaedec9d91ec7f1015ab719ed603ccb69ee4abe
Jeff Sember <jpsember@gmail.com>
2024-01-24 15:41:30 -0800

    Issue #23

--- src/main/java/js/leetcode/MinimumHeightTrees.java
+++ src/main/java/js/leetcode/MinimumHeightTrees.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Stack;
 
 public class MinimumHeightTrees extends LeetCode {
 
@@ -43,52 +42,51 @@ public class MinimumHeightTrees extends LeetCode {
 
       // Perform Floyd-Warshall algorithm.
 
-      // Construct edge incidence matrix.   
-      mAdjMat = new int[n][n];
-      mPathMat = new int[n][n];
-      for (var row : mAdjMat)
+      // Construct edge incidence matrix, and path matrix.  
+      var dist = new int[n][n]; // Holds the shortest known number of edges between i and j
+      var path = new int[n][n]; // Holds intermediate destination on path from i to j (j if single step)
+
+      for (var row : dist)
         Arrays.fill(row, Integer.MAX_VALUE);
-      for (int i = 0; i < n; i++) {
-        setEdge(i, i, 0);
-      }
+      for (int i = 0; i < n; i++)
+        dist[i][i] = 0;
+
       if (db) {
-        for (var row : mPathMat)
+        for (var row : path)
           Arrays.fill(row, Integer.MIN_VALUE);
       }
 
       for (var edge : edges) {
         var a = edge[0];
         var b = edge[1];
-        setEdge(a, b, 1);
-        setEdge(b, a, 1);
-        setPath(a, b, b);
-        setPath(b, a, a);
+        dist[a][b] = 1;
+        dist[b][a] = 1;
+        path[a][b] = b;
+        path[b][a] = a;
       }
-      db("adjmat:", INDENT, strTable(mAdjMat));
+      db("adjmat:", INDENT, strTable(dist));
 
       {
         for (int k = 0; k < n; k++) {
           for (int i = 0; i < n; i++) {
-            var distIK = getEdge(i, k);
+            var distIK = dist[i][k];
             if (distIK == Integer.MAX_VALUE)
               continue;
             for (int j = 0; j < n; j++) {
-              var distKJ = getEdge(k, j);
+              var distKJ = dist[k][j];
               if (distKJ == Integer.MAX_VALUE)
                 continue;
-              var distIJ = getEdge(i, j);
+              var distIJ = dist[i][j];
               var newdist = distIK + distKJ;
               if (newdist < distIJ) {
-                pr("dist from", i, "to", j, "improving from", distIJ, "to", newdist);
-                setEdge(i, j, newdist);
-                pr("path from", i, "to", j, "now goes through", k);
-                setPath(i, j, k);
+                dist[i][j] = newdist;
+                path[i][j] = k;
               }
             }
           }
         }
       }
-      db("adjmat:", INDENT, strTable(mAdjMat));
+      db("adjmat:", INDENT, strTable(dist));
 
       // Set k = graph radius (the maximum distance between any two vertices), and save all pairs
       // of vertices that are separated by this distance
@@ -98,7 +96,7 @@ public class MinimumHeightTrees extends LeetCode {
       var k = 0;
       for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-          var k2 = mAdjMat[i][j];
+          var k2 = dist[i][j];
           if (k2 > k) {
             diam.clear();
             k = k2;
@@ -115,54 +113,45 @@ public class MinimumHeightTrees extends LeetCode {
       // added to the answer list.
 
       var result = new HashSet<Integer>();
-      var steps = new int[k + 1];
+      // var steps = new int[k + 1];
       int ki0 = k / 2;
+      int ki1 = ki0 + (k & 1);
+
       for (int j = 0; j < diam.size(); j += 2) {
         var a = diam.get(j);
         var b = diam.get(j + 1);
+
         db("walking from", a, "to", b);
 
         var posn = a;
-        int cursor = 0;
-        steps[cursor++] = posn;
+        int cursor = 1; // already at start
 
         while (posn != b) {
-          int c = b;
-          while (true) {
-            var c2 = mPathMat[posn][c];
-            if (c == c2)
-              break;
-            c = c2;
-          }
-          posn = c;
-          steps[cursor++] = posn;
-        }
 
-        db("steps:", str(steps));
-        result.add(steps[ki0]);
-        if ((k & 1) != 0)
-          result.add(steps[ki0 + 1]);
+          // Advance posn to next *single step* towards b
+          int dest = b;
+          while (true) {
+            // Get next step along path
+            var firstStepToDest = path[posn][dest];
+            if (dest == firstStepToDest) // If this is a single step, stop
+              break;
+            dest = firstStepToDest;
+          }
+          posn = dest;
+
+          // If this position is halfway, add to result
+          if (cursor >= ki0)
+            result.add(posn);
+          // If remaining steps are past halfway, stop
+          if (cursor == ki1)
+            break;
+
+          cursor++;
+        }
       }
 
       return new ArrayList<Integer>(result);
     }
-
-    private void setEdge(int a, int b, int distance) {
-      mAdjMat[a][b] = distance;
-      //  mAdjMat[b][a] = distance;
-    }
-
-    private void setPath(int src, int dest, int nextVertex) {
-      //db("set path", src, "=>", dest, "next vert:", nextVertex);
-      mPathMat[src][dest] = nextVertex;
-    }
-
-    private int getEdge(int a, int b) {
-      return mAdjMat[a][b];
-    }
-
-    private int[][] mAdjMat;
-    private int[][] mPathMat;
 
   }
 }
