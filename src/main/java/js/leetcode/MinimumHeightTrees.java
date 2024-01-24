78a8b8fd18609fa036817c556e29ffc20f8c7748
Jeff Sember <jpsember@gmail.com>
2024-01-24 15:26:24 -0800

    Issue #23

--- src/main/java/js/leetcode/MinimumHeightTrees.java
+++ src/main/java/js/leetcode/MinimumHeightTrees.java
@@ -4,7 +4,9 @@ import static js.base.Tools.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Stack;
 
 public class MinimumHeightTrees extends LeetCode {
 
@@ -13,9 +15,10 @@ public class MinimumHeightTrees extends LeetCode {
   }
 
   public void run() {
+    //x(3, "[[0,1],[0,2]]");
     // x(4, "[[1,0],[1,2],[1,3]]");
+    // x(4, "1 3 3 2 2 0");
     // x(6, "[[3,0],[3,1],[3,2],[3,4],[5,4]]");
-    // x(3, "[[0,1],[0,2]]");
     x(6, "[[3,0],[3,1],[3,2],[3,4],[5,4]]");
   }
 
@@ -42,11 +45,25 @@ public class MinimumHeightTrees extends LeetCode {
 
       // Construct edge incidence matrix.   
       mAdjMat = new int[n][n];
+      mPathMat = new int[n][n];
       for (var row : mAdjMat)
         Arrays.fill(row, Integer.MAX_VALUE);
+      for (int i = 0; i < n; i++) {
+        setEdge(i, i, 0);
+      }
+      if (db) {
+        for (var row : mPathMat)
+          Arrays.fill(row, Integer.MIN_VALUE);
+      }
 
-      for (var edge : edges)
-        setEdge(edge[0], edge[1], 1);
+      for (var edge : edges) {
+        var a = edge[0];
+        var b = edge[1];
+        setEdge(a, b, 1);
+        setEdge(b, a, 1);
+        setPath(a, b, b);
+        setPath(b, a, a);
+      }
       db("adjmat:", INDENT, strTable(mAdjMat));
 
       {
@@ -61,71 +78,91 @@ public class MinimumHeightTrees extends LeetCode {
                 continue;
               var distIJ = getEdge(i, j);
               var newdist = distIK + distKJ;
-              if (newdist < distIJ)
+              if (newdist < distIJ) {
+                pr("dist from", i, "to", j, "improving from", distIJ, "to", newdist);
                 setEdge(i, j, newdist);
+                pr("path from", i, "to", j, "now goes through", k);
+                setPath(i, j, k);
+              }
             }
           }
         }
       }
       db("adjmat:", INDENT, strTable(mAdjMat));
 
-      // Set k = graph radius (the maximum distance between any two vertices)
+      // Set k = graph radius (the maximum distance between any two vertices), and save all pairs
+      // of vertices that are separated by this distance
+
+      List<Integer> diam = new ArrayList<>();
+
       var k = 0;
       for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-          k = Math.max(k, mAdjMat[i][j]);
+          var k2 = mAdjMat[i][j];
+          if (k2 > k) {
+            diam.clear();
+            k = k2;
+          }
+          if (k == k2) {
+            diam.add(i);
+            diam.add(j);
+          }
         }
       }
       db("diameter, k:", k);
 
-      // While k > 1, delete all vertices that are at a distance k from any other 
-      // (still existing) vertex, and subtract 2 from k.  We are 'peeling an onion' of
-      // diameter pairs.
+      // For each pair of diameter vertices (u,v), the node(s) halfway along the path between these two are 
+      // added to the answer list.
 
-      var dead = new boolean[n];
+      var result = new HashSet<Integer>();
+      var steps = new int[k + 1];
+      int ki0 = k / 2;
+      for (int j = 0; j < diam.size(); j += 2) {
+        var a = diam.get(j);
+        var b = diam.get(j + 1);
+        db("walking from", a, "to", b);
 
-      while (k > 1) {
-        db("deleting vertices separated by k=", k);
-        for (int i = 0; i < n; i++) {
-          if (dead[i])
-            continue;
-          for (int j = i + 1; j < n; j++) {
-            if (dead[j])
-              continue;
-            var dist = mAdjMat[i][j];
-            if (dist == k) {
-              dead[i] = true;
-              dead[j] = true;
-              db("...deleting vertices:", i, j);
-            }
+        var posn = a;
+        int cursor = 0;
+        steps[cursor++] = posn;
+
+        while (posn != b) {
+          int c = b;
+          while (true) {
+            var c2 = mPathMat[posn][c];
+            if (c == c2)
+              break;
+            c = c2;
           }
+          posn = c;
+          steps[cursor++] = posn;
         }
-        k -= 2;
+
+        db("steps:", str(steps));
+        result.add(steps[ki0]);
+        if ((k & 1) != 0)
+          result.add(steps[ki0 + 1]);
       }
 
-      List<Integer> result = new ArrayList<>();
-      for (int i = 0; i < n; i++)
-        if (!dead[i])
-          result.add(i);
-
-      return result;
+      return new ArrayList<Integer>(result);
     }
 
     private void setEdge(int a, int b, int distance) {
-      if (a < b)
-        mAdjMat[a][b] = distance;
-      else
-        mAdjMat[b][a] = distance;
+      mAdjMat[a][b] = distance;
+      //  mAdjMat[b][a] = distance;
+    }
+
+    private void setPath(int src, int dest, int nextVertex) {
+      //db("set path", src, "=>", dest, "next vert:", nextVertex);
+      mPathMat[src][dest] = nextVertex;
     }
 
     private int getEdge(int a, int b) {
-      if (a < b)
-        return mAdjMat[a][b];
-      else
-        return mAdjMat[b][a];
+      return mAdjMat[a][b];
     }
 
     private int[][] mAdjMat;
+    private int[][] mPathMat;
 
   }
 }
