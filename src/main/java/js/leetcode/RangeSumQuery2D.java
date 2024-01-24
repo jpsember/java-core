2d6ddc739caedc6319086002856fe0d321768750
Jeff Sember <jpsember@gmail.com>
2024-01-24 10:55:10 -0800

    Issue #23

--- src/main/java/js/leetcode/RangeSumQuery2D.java
+++ src/main/java/js/leetcode/RangeSumQuery2D.java
@@ -10,60 +10,41 @@ public class RangeSumQuery2D extends LeetCode {
 
   public void run() {
 
-    x("[[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]]", 5, 2, 1, 4,
-        3);
+    x("[[[[-4,-5]]]", 2, 0, 0, 0, 1);
+    //[[[[-4,-5]]],[0,0,0,0],[0,0,0,1],[0,1,0,1]]
 
   }
 
   private void x(String ns, int width, int r1, int c1, int r2, int c2) {
-
     var nums = extractMatrix(ns, width);
     var alg1 = new NumMatrix(nums);
     var sum = alg1.sumRegion(r1, c1, r2, c2);
     pr(sum);
-
   }
 
   class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
-      int h = matrix.length;
-      int w = matrix[0].length;
-      var s2 = new int[h][w];
-      s2[0][0] = matrix[0][0];
 
-      sum = new int[h + 1][w + 1];
-      for (int scan = 1;; scan++) {
-        int y = scan;
-        int x = 0;
-        if (y >= h) {
-          x = y - h;
-          y = h - 1;
-          if (x == w)
-            break;
-        }
+      // The 'sum' array will hold sum of cells(0...x-1, 0...y-1); so that first row and col are zero
 
-        while (x < w && y >= 0) {
-          var prev1 = 0;
-          var prev2 = 0;
-          var prev3 = 0;
-          if (x > 0) {
-            prev1 = s2[y][x - 1];
-            if (y > 0)
-              prev3 = s2[y - 1][x - 1];
-          }
-          if (y > 0)
-            prev2 = s2[y - 1][x];
-          s2[y][x] = prev1 + prev2 - prev3 + matrix[y][x];
-          sum[y + 1][x + 1] = s2[y][x];
-          x++;
-          y--;
-        }
+      int h = matrix.length + 1;
+      int w = matrix[0].length + 1;
+      sum = new int[h][w];
+
+      for (int y = 1; y < h; y++) {
+        var ry = sum[y];
+        var rym = sum[y - 1];
+        var mym = matrix[y - 1];
+        for (int x = 1; x < w; x++)
+          ry[x] = mym[x - 1] + ry[x - 1] + rym[x] - rym[x - 1];
       }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-      return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] - sum[row2 + 1][col1] + sum[row1][col1];
+      var r2 = sum[row2 + 1];
+      var r1 = sum[row1];
+      return r2[col2 + 1] - r1[col2 + 1] - r2[col1] + r1[col1];
     }
 
     private int[][] sum;
