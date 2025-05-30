cf362d0ec45d7b1c92dbfce2bba0aa9c5d6d748f
jeff <jeff@urbanlogiq.com>
2025-05-30 15:19:17 -0700

    Issue #29: will update describe method later

--- src/main/java/js/parsing/DFA.java
+++ src/main/java/js/parsing/DFA.java
@@ -167,4 +167,128 @@ public class DFA {
   public int numTokens() {
     return mTokenNames.length;
   }
+
+
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * Get a description of the DFA; for development purposes only
+   */
+  public JSMap describe() {
+    return map().put("", "<not supported since new dfa version>");
+  }
+
+//  /**
+//   * Get a description of the DFA; for development purposes only
+//   */
+//  public JSMap describe() {
+//    var m = map();
+//    for (var s : mStates) {
+//      var stateKey = "" + s.debugId();
+//      var altKey = stateKey + "*";
+//
+//      if (m.containsKey(stateKey) || m.containsKey(altKey)) {
+//        m.putNumbered(stateKey, "*** duplicate state id ***");
+//        continue;
+//      }
+//      var edgeMap = map();
+//      m.put(s.finalState() ? altKey : stateKey, edgeMap);
+//      for (var edge : s.edges()) {
+//        var ds = edge.destinationState();
+//        String edgeKey =
+//            ds.finalState() ? "*  " : String.format("%3d", edge.destinationState().debugId());
+//        if (edgeMap.containsKey(edgeKey))
+//          edgeMap.put("**ERR** " + edge.destinationState().debugId(), "duplicate destination state");
+//        else {
+//          edgeMap.putUnsafe(edgeKey, edgeDescription(edge));
+//        }
+//      }
+//    }
+//    return m;
+//  }
+//
+//  private static Object edgeProblem(Edge edge, String message) {
+//    return "*** problem with edge: " + message + " ***; " + JSList.with(edge.codeSets());
+//  }
+//
+//  /**
+//   * Get a description of an edge's code sets, for display as a map key
+//   */
+//  private Object edgeDescription(Edge edge) {
+//    var cs = edge.codeSets();
+//    if (cs.length % 2 != 0) {
+//      return edgeProblem(edge, "odd number of elements");
+//    }
+//    StringBuilder sb = new StringBuilder();
+//    for (var i = 0; i < cs.length; i += 2) {
+//      var a = cs[i];
+//      var b = cs[i + 1];
+//      if ((a < 0 != b < 0) || (a >= b)) {
+//        return edgeProblem(edge, "illegal code set");
+//      }
+//      if (a == 0)
+//        return edgeProblem(edge, "illegal code set");
+//      if (a < 0) {
+//        if (b != a + 1 || cs.length != 2) {
+//          return edgeProblem(edge, "unexpected token id expr");
+//        }
+//        var tokenId = -b - 1;
+//        if (tokenId < 0 || tokenId >= mTokenNames.length) {
+//          return "*** no such token id: " + tokenId;
+//        }
+//        return "<" + tokenName(tokenId) + ">";
+//      }
+//
+//      if (b == a + 1) {
+//        append(sb, charExpr(a));
+//        continue;
+//      }
+//
+//      var maxRun = (b == 256) ? 2 : 5;
+//      int skipStart = 1000;
+//      int skipEnd = 1000;
+//      if (b - a > 2 * maxRun + 4) {
+//        skipStart = a + maxRun;
+//        skipEnd = b - 1 - maxRun;
+//      }
+//
+//      for (int j = a; j < b; j++) {
+//        if (j < skipStart || j > skipEnd) {
+//          append(sb, charExpr(j));
+//        } else if (j == skipStart) {
+//          append(sb, "...");
+//        }
+//      }
+//    }
+//    return sb.toString();
+//  }
+//
+//  private static void append(StringBuilder sb, Object expr) {
+//    if (sb.length() > 0 && sb.charAt(sb.length() - 1) > ' ')
+//      sb.append(' ');
+//    sb.append(expr);
+//  }
+//
+//  /**
+//   * Construct a string representation of a character code
+//   */
+//  private static String charExpr(int n) {
+//    switch (n) {
+//      case 0x0a:
+//        return "_LF";
+//      case 0x09:
+//        return "_HT";
+//      case 0x0d:
+//        return "_CR";
+//      case 0x20:
+//        return "_SP";
+//      default:
+//        if (n > 32 && n < 128) {
+//          return Character.toString((char) n);
+//        }
+//        return String.format("%02x", n);
+//    }
+//  }
+//
+
 }
